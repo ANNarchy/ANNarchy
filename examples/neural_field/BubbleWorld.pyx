f7a4345e4a899cbf580ac46ff755b1fc3cc99b30
Julien Vitay <julien.vitay@informatik.tu-chemnitz.de>
2013-07-31 21:33:59 +0200

    Added an example in NeuralField on how to define the world in Cython. Measure?

---
+++ examples/neural_field/BubbleWorld.pyx
@@ -0,0 +1,40 @@
+from NeuralField import *
+import numpy as np
+cimport numpy as np
+from datetime import datetime
+
+def run(InputPop, FocusPop):
+
+    cdef int w = 20
+    cdef int h = 20
+    
+    cdef float angle = 0.0
+    cdef float radius = 0.5
+    cdef float sigma = 2.0
+    
+    
+    cdef int i, idx
+    cdef float cw, ch, dist, value
+
+    cdef np.ndarray data = np.zeros(w*h)
+
+    for i in xrange (10000):
+        t_start = datetime.now()
+
+        angle += 1.0/5000.0
+
+        cw = w / 2.0 * ( 1.0 + radius * np.cos(2 * math.pi * angle ) )
+        ch = h / 2.0 * ( 1.0 + radius * np.sin(2 * math.pi * angle ) )
+
+        for x in xrange(w):
+            for y in xrange(h):
+                dist = (x-cw)**2 + (y-ch)**2
+                value = 0.5 * np.exp(-dist/2.0/sigma**2)
+                idx = x+y*w
+                data[idx] = value
+
+        InputPop.cyInstance.baseline = data
+        
+        Simulate(1)
+        t_end = datetime.now()
+        print t_end-t_start
--- examples/neural_field/NeuralField.py
+++ examples/neural_field/NeuralField.py
@@ -45,47 +45,13 @@ Proj2 = Projection( pre = "Focus", post = "Focus", target = 'inh',
                     connector = Connector( type='DoG', weights=RandomDistribution('uniform', [0,1]), 
                                            amp_pos=0.2, sigma_pos=0.1, amp_neg=0.1, sigma_neg=0.7 ) )
 
-#
-# Analyse and compile everything, initialize the parameters/variables...
-#
-Compile()
-
-
-#
 # Main program
-#
 if __name__ == "__main__":
+
+    # Analyse and compile everything, initialize the parameters/variables...
+    Compile()
     
-    import numpy as np
-
-
-#    plotData = [{'pop': InputPop, 'var': 'rate', 'name':'input.rate'}, {'pop': FocusPop, 'var': 'rate', 'name': 'focus.rate'}]
-    #plotData = [{'proj': Proj2, 'ranks': np.arange(0,10), 'name': 'Proj2'}]
+    import pyximport; pyximport.install()
+    import BubbleWorld
     
-    w = 20
-    h = 20
-    data = np.zeros((1,w*h))
-    
-    angle = 0.0
-    radius = 0.5
-    sigma = 2.0
-
-#    PlotThread(plotData, True)
-
-    for i in xrange (10000):
-
-        angle += 1.0/5000.0
-
-        cw = w / 2.0 * ( 1.0 + radius * np.cos(2 * math.pi * angle ) )
-        ch = h / 2.0 * ( 1.0 + radius * np.sin(2 * math.pi * angle ) )
-
-        for x in xrange(w):
-            for y in xrange(h):
-                dist = (x-cw)**2 + (y-ch)**2
-                value = 0.5 * np.exp(-dist/2.0/sigma**2)
-                idx = x+y*w
-                data[(0,idx)] = value
-
-        InputPop.cyInstance.baseline = data[0]
-        Simulate(1)
-        print FocusPop.cyInstance.rate.shape
+    BubbleWorld.run(InputPop, FocusPop)
