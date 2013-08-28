d3b863517d8cc7df70c3ea83f38bea203b7068ce
Helge Dinkelbach <hdinkelbach@gmail.com>
2013-08-28 11:36:09 +0200

    Added min, max as new optional properties.

--- ANNarchy4/core/Variable.py
+++ ANNarchy4/core/Variable.py
@@ -3,13 +3,19 @@ class Variable:
 
         self.eq = None
         self.init = None
-
+        self.min = None
+        self.max = None
+        
         for key in keyValueArgs:
 
             if key == 'eq':
                 self.eq = keyValueArgs[key]
             elif key=='init':
                 self.init = keyValueArgs[key]
+            elif key=='min':
+                self.min = keyValueArgs[key]
+            elif key=='max':
+                self.max = keyValueArgs[key]
             else:
                 print 'unknown key: '+key
          
