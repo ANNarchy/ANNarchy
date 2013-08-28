c32f36fb701425ccf5e28c833b98111d4982c6ff
Helge Dinkelbach <hdinkelbach@gmail.com>
2013-08-28 17:52:18 +0200

    Fixed multi neuron example.

--- examples/multi_neuron/MultiNeuron.py
+++ examples/multi_neuron/MultiNeuron.py
@@ -7,35 +7,36 @@ from ANNarchy4 import *
 #
 # Define the neuron classes
 #
-Input = Neuron(   tau = 1.0,
-                  rate = Variable(init = 0.0)
-               )
+Input = Neuron(   
+    tau = 1.0,
+    rate = Variable(init = 0.0)
+)
 
-Layer1 = Neuron(   tau = 10.0,
-                   mp = Variable(init=0.0, eq="tau * dmp / dt + mp = sum(exc)"),
-                   rate = Variable(init=0.0, eq="rate = pos(mp)"),
-                   order = ['mp', 'rate']
-  	           )
+Layer1 = Neuron(   
+    tau = 10.0,
+    mp = Variable(init=0.0, eq="tau * dmp / dt + mp = sum(exc)"),
+    rate = Variable(init=0.0, eq="rate = pos(mp)"),
+    order = ['mp', 'rate']
+)
 
-Layer2 = Neuron(   tau = 10.0,
-                   mp = Variable(init=0.0, eq="tau * dmp / dt + mp = sum(exc) - sum(inh)"),
-                   rate = Variable(init=0.0, eq="rate = pos(mp)"),
-                   order = ['mp', 'rate']
-               )
+Layer2 = Neuron(
+    tau = 10.0,
+    mp = Variable(init=0.0, eq="tau * dmp / dt + mp = sum(exc) - sum(inh)"),
+    rate = Variable(init=0.0, eq="rate = pos(mp)"),
+    order = ['mp', 'rate']
+)
 
-Oja = Synapse(tau = 5000,
-              dt = 1.0,
-              alpha = 8.0,
-              value = Variable(init=0.0, eq = "tau * dvalue / dt = pre.rate*post.rate - alpha * post.rate^2 *value"),
-              )
+Oja = Synapse(
+    tau = 2000,
+    alpha = 8.0,
+    value = Variable(init=0.0, eq = "tau * dvalue / dt = pre.rate*post.rate - alpha * post.rate^2 *value"),
+)
 
-AntiHebb = Synapse(tau = 5000,
-                   dt = 50.0,
-                   alpha = 0.3,
-                   w = Variable(init=0.0, eq = "tau * dw / dt = pre.rate*post.rate - alpha * post.rate * w"),
-                   value = Variable(init=0.0, eq="value = pos(w)"),
-                   order = ['w','value']
-                  )
+AntiHebb = Synapse(
+    tau = 2000,
+    alpha = 0.3,
+    value = Variable(init=0.0, eq = "tau * dvalue / dt = pre.rate*post.rate - alpha * post.rate * value", min=0.0)
+)
 
 InputPop = Population("Input", (8,8,1), Input)
 Layer1Pop = Population("Layer1", (8,8,1), Layer1)
@@ -49,9 +50,6 @@ Proj_L2_L2 = Projection(pre="Layer2", post="Layer2", target='inh', synapse=AntiH
 # Analyse and compile everything, initialize the parameters/variables...
 #
 compile()
-
-import math
-import numpy as np
 
 if __name__ == '__main__':
 
@@ -67,12 +65,12 @@ if __name__ == '__main__':
         # appears a vertical bar?
         for i in xrange(8):
             if np.random.rand(1) < 1.0/8.0:
-               bars[:,i] = 1.0
+                bars[:,i] = 1.0
 
         # appears a horizontal bar?
         for i in xrange(8):
             if np.random.rand(1) < 1.0/8.0:
-               bars[i,:] = 1.0
+                bars[i,:] = 1.0
 
         InputPop.rate = bars.reshape(8*8)
 
