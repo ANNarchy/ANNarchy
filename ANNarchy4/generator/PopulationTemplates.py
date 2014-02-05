Added additional barriers after computation steps. Global operations are computed by master. Minor bug fixes.

ANNarchy4/generator/PopulationTemplates.py
@@ -31,7 +31,9 @@ public:
     
     void resetToInit();
     
-    void metaStep();
+    void localMetaStep(int neur_rank);
+    
+    void globalMetaStep();
     
     void globalOperations();
     
@@ -85,7 +87,9 @@ public:
     
     void resetToInit();
     
-    void metaStep();
+    void localMetaStep(int neur_rank);
+    
+    void globalMetaStep();
     
     void globalOperations();
     
@@ -198,8 +202,12 @@ void %(class)s::resetToInit() {
 %(resetToInit)s
 }
 
-void %(class)s::metaStep() {
-%(metaStep)s    
+void %(class)s::localMetaStep(int i) {
+%(localMetaStep)s
+}
+
+void %(class)s::globalMetaStep() {
+%(globalMetaStep)s        
 }
 
 void %(class)s::globalOperations() {
@@ -256,12 +264,20 @@ void %(class)s::resetToInit() {
 %(resetToInit)s
 }
 
-void %(class)s::metaStep() 
-{
-%(metaStep)s
+void %(class)s::localMetaStep(int i) {
+%(localMetaStep)s    
+}
+
+void %(class)s::globalMetaStep() {
+%(globalMetaStep)s    
 }
 
 void %(class)s::globalOperations() {
+    
+    propagateSpike();
+    
+    reset();
+
 %(global_ops)s
 }
 
@@ -283,8 +299,11 @@ void %(class)s::reset() {
 
     if (!reset_.empty())
     {
+        for (auto it = reset_.begin(); it != reset_.end(); it++)
+        {
 %(reset_event)s
-
+        }
+        
         reset_.erase(reset_.begin(), reset_.end());
     }
     
@@ -375,6 +394,8 @@ cdef extern from "../build/%(name)s.h":
         
         string getName()
         
+        vector[ vector[int] ] getSpikeTimings()
+        
         void resetToInit()
         
         void setMaxDelay(int)
@@ -391,6 +412,9 @@ cdef class py%(name)s:
 
     def name(self):
         return self.cInstance.getName()
+
+    def get_spike_timings(self):
+        return np.array( self.cInstance.getSpikeTimings() )
 
     def reset(self):
         self.cInstance.resetToInit()

examples/izhikevich/Izhikevitch.py
@@ -4,7 +4,7 @@ from pylab import show, figure, subplot, legend, close
 #
 # experiment setup
 setup(dt=1)
-nb_steps = 10000
+nb_steps = 1000
 nb_exc_neurons = 800
 nb_inh_neurons = 200
 
@@ -48,57 +48,51 @@ Simple = SpikeSynapse(
 )
 
 Excitatory = Population(name='Excitatory', geometry=(800), neuron=Izhikevitch)
-#===============================================================================
-# re = np.random.random(nb_exc_neurons)
-# Excitatory.c = -65.0 + 15.0*re**2
-# Excitatory.d = 8.0 - 6.0*re**2
-#===============================================================================
+re = np.random.random(nb_exc_neurons)
+Excitatory.c = -65.0 + 15.0*re**2
+Excitatory.d = 8.0 - 6.0*re**2
 
 Inhibitory = Population(name='Inhibitory', geometry=(200), neuron=Izhikevitch)
-#===============================================================================
-# ri = np.random.random(nb_inh_neurons)
-# Inhibitory.noise_scale=2.0
-# Inhibitory.b = 0.25 - 0.05*ri
-# Inhibitory.a = 0.02 + 0.08*ri
-# Inhibitory.u = (0.25 - 0.05*ri) * (-65.0) # b * -65
-#===============================================================================
+ri = np.random.random(nb_inh_neurons)
+Inhibitory.noise_scale=2.0
+Inhibitory.b = 0.25 - 0.05*ri
+Inhibitory.a = 0.02 + 0.08*ri
+Inhibitory.u = (0.25 - 0.05*ri) * (-65.0) # b * -65
 
-#===============================================================================
-# exc_exc = Projection(
-#     pre=Excitatory, 
-#     post=Excitatory, 
-#     target='exc',
-#     synapse = Simple,
-#     connector=All2All(weights=Uniform(0,0.5))
-# )
-#   
-# exc_inh = Projection(
-#     pre=Excitatory, 
-#     post=Inhibitory, 
-#     target='exc',
-#     synapse = Simple,
-#     connector=All2All(weights=Uniform(0,0.5))
-# )
-#  
-# inh_exc = Projection(
-#     pre=Inhibitory, 
-#     post=Excitatory, 
-#     target='inh',
-#     synapse = Simple,
-#     connector=All2All(weights= Uniform(-1.0,0.0))
-# )
-#  
-# inh_inh = Projection(
-#     pre=Inhibitory, 
-#     post=Inhibitory, 
-#     target='inh',
-#     synapse = Simple,
-#     connector=All2All(weights=Uniform(-1.0,0.0))
-# )
-#===============================================================================
+exc_exc = Projection(
+    pre=Excitatory, 
+    post=Excitatory, 
+    target='exc',
+    synapse = Simple,
+    connector=All2All(weights=Uniform(0,0.5))
+)
+   
+exc_inh = Projection(
+    pre=Excitatory, 
+    post=Inhibitory, 
+    target='exc',
+    synapse = Simple,
+    connector=All2All(weights=Uniform(0,0.5))
+)
+  
+inh_exc = Projection(
+    pre=Inhibitory, 
+    post=Excitatory, 
+    target='inh',
+    synapse = Simple,
+    connector=All2All(weights= Uniform(-1.0,0.0))
+)
+  
+inh_inh = Projection(
+    pre=Inhibitory, 
+    post=Inhibitory, 
+    target='inh',
+    synapse = Simple,
+    connector=All2All(weights=Uniform(-1.0,0.0))
+)
 
 # Compile
-compile(debug_build = True)
+compile()
 
 def plot(population, data):
     """
@@ -178,10 +172,9 @@ if __name__ == '__main__':
     for i in xrange(nb_steps):
         
         # first 20 ms no input
-        Excitatory.I_in = I[i,:]   
-        print 'py: start' 
+        Excitatory.I_in = I[i,:]
+        print i
         simulate(1)
-        print 'py: stop'
         
     data = get_record( to_record )
     