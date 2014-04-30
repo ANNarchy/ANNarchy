427ad60469135896b8ba041022aee1b88df22fb9
Julien Vitay <julien.vitay@informatik.tu-chemnitz.de>
2014-04-30 14:55:18 +0200

    doc does not depend on cloud theme anymore.

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,6 @@ extensions = [  'sphinx.ext.autodoc',
                 'sphinx.ext.coverage', 
                 'sphinx.ext.mathjax', 
                 'sphinx.ext.viewcode',  
-                'cloud_sptheme.ext.index_styling'
              ]
 
 # Add any paths that contain templates here, relative to this directory.
