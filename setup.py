378879fba0056f3953d5f97cca9a85d464c0eb80
Julien Vitay <julien.vitay@informatik.tu-chemnitz.de>
2014-06-20 14:25:20 +0200

    Started version 4.2.2

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ except:
 ################################################
 print('Installing ANNarchy on your system')
 setup(  name='ANNarchy',
-		version='4.2.1',
+		version='4.2.2',
 		license='GPLv2 or later',
 		platforms='GNU/Linux',
 		description='Artificial Neural Networks architect',
