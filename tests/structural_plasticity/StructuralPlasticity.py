e7c9f21fad446578167d075ada405669486de72e
Julien Vitay <julien.vitay@informatik.tu-chemnitz.de>
2014-09-26 16:17:46 +0200

    Restored the test example for structural plasticity.

--- tests/structural_plasticity/StructuralPlasticity.py
+++ tests/structural_plasticity/StructuralPlasticity.py
@@ -3,19 +3,29 @@
 #
 from ANNarchy import *
 
-setup(num_threads=1)
+setup(num_threads=1, structural_plasticity=True)
 
 # Defining the neurons
-InputNeuron = RateNeuron(
+InputNeuron = Neuron(
     equations="""
-        r = 0.2 : init = 0.1 
-    """
+        r += 0.2 : init = 0.0
+    """,
+    spike="r>0.1",
+    reset="r=0.0"
+
 )
 
-OutputNeuron = RateNeuron(
+OutputNeuron = Neuron(
     equations="""
-        r = sum(exc)
-    """
+        r = g_exc
+    """,
+    spike="r>100.0"
+)
+
+DefaultSynapse = Synapse(
+    parameters = "toto = 1.0",
+    equations = "alpha= 1.0: init=2.0",
+    pre_spike = "g_target += w; w += 1.0"
 )
 
 NEURON = 30
@@ -24,20 +34,43 @@ CONN = 10
 input_pop = Population(geometry=(NEURON), neuron=InputNeuron)
 output_pop = Population(geometry=(1), neuron=OutputNeuron)
 
-proj = Projection(input_pop, output_pop, 'exc').connect_fixed_number_pre(CONN, 1.0)
+proj = Projection(input_pop, output_pop, 'exc', synapse = DefaultSynapse).connect_fixed_number_pre(CONN, 1.0)
 
-compile() # needed to save connectivity matrix
+compile() 
 
-print proj.dendrite(0).rank
+proj.alpha = 3.0
 
-rank = raw_input('Add connection to: ')
+print 'Rank:', proj.dendrite(0).rank
+print 'Weights:', proj.dendrite(0).w
+print 'Alpha:', proj.dendrite(0).alpha
 
-proj.dendrite(0).add_synapse(int(rank), 1.0)
+dendrite = proj.dendrite(0)
 
-print proj.dendrite(0).rank
+synapse = dendrite.synapse(proj.dendrite(0).rank[2])
 
-rank = raw_input('Remove connection to: ')
+synapse.w = 10.0
 
-proj.dendrite(0).remove_synapse(int(rank))
+for synapse in proj.dendrite(0):
+    print synapse.rank, synapse.w
 
-print proj.dendrite(0).rank
+print 'Rank:', proj.dendrite(0).rank
+print 'Weights:', proj.dendrite(0).w
+print 'Alpha:', proj.dendrite(0).alpha
+
+rank = int(raw_input('Add connection to: '))
+proj.dendrite(0).add_synapse(rank, 2.0)
+
+
+print 'Rank:', proj.dendrite(0).rank
+print 'Weights:', proj.dendrite(0).w
+print 'Alpha:', proj.dendrite(0).alpha
+
+rank = int(raw_input('Remove connection to: '))
+
+proj.dendrite(0).remove_synapse(rank)
+
+print 'Rank:', proj.dendrite(0).rank
+print 'Weights:', proj.dendrite(0).w
+print 'Alpha:', proj.dendrite(0).alpha
+
+simulate(10)
