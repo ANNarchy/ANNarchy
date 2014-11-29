e2b34e07fcab5e79418313c98a82a93f4333458b
Julien Vitay <julien.vitay@informatik.tu-chemnitz.de>
2014-11-29 14:44:13 +0100

    Separated code generation for projections.

--- ANNarchy/generator/OMP/PopulationGenerator.py
+++ ANNarchy/generator/OMP/PopulationGenerator.py
@@ -127,7 +127,7 @@ struct PopStruct%(id)s{
         code += """
 };    
 """   
-        return code  
+        return code  % {'id': pop.id}
 
 
 #######################################################################
