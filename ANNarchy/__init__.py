f97a51626efe4b9385ec27be1ca4892465730569
Julien Vitay <julien.vitay@informatik.tu-chemnitz.de>
2015-01-05 15:57:09 +0100

    Finished version 4.3.3.

--- ANNarchy/__init__.py
+++ ANNarchy/__init__.py
@@ -26,6 +26,6 @@ import os, sys
 
 # Version
 __version__ = '4.3'
-__release__ = '4.3.2'
+__release__ = '4.3.3'
 core.Global._print( 'ANNarchy ' + __version__ + ' (' + __release__ + \
                    ') on ' + sys.platform + ' (' + os.name + ').' )
