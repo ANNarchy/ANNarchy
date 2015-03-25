5b24fb1bb5539e8211044b647940dd718fe5bb6d
Julien Vitay <julien.vitay@informatik.tu-chemnitz.de>
2015-03-25 16:41:32 +0100

    Added Vgels-Abbott benchmark to examples.

---
+++ examples/vogels_abbott/COBA.py
@@ -0,0 +1,70 @@
+from ANNarchy import *
+
+dt = 0.1
+setup(dt=dt)
+
+# ###########################################
+# Neuron model
+# ###########################################
+COBA = Neuron(
+    parameters="""
+        El = -60.0          : population
+        Vr = -60.0          : population
+        Erev_exc = 0.0      : population
+        Erev_inh = -80.0    : population
+        Vt = -50.0          : population
+        tau = 20.0          : population
+        tau_exc = 5.0       : population
+        tau_inh = 10.0      : population
+        I = 20.0            : population
+    """,
+    equations="""
+        tau * dv/dt = (El - v) + g_exc * (Erev_exc - v) + g_inh * (Erev_inh - v ) + I
+
+        tau_exc * dg_exc/dt = - g_exc 
+        tau_inh * dg_inh/dt = - g_inh 
+    """,
+    spike = "v > Vt",
+    reset = "v = Vr",
+    refractory = 5.0
+)
+
+# ###########################################
+# Create population
+# ###########################################
+P = Population(geometry=4000, neuron=COBA)
+Pe = P[:3200]
+Pi = P[3200:]
+P.v = Normal(-55.0, 5.0)
+P.g_exc = Normal(4.0, 1.5)
+P.g_inh = Normal(20.0, 12.0)
+
+# ###########################################
+# Connect the network
+# ###########################################
+Ce = Projection(pre=Pe, post=P, target='exc')
+Ce.connect_fixed_probability(weights=0.6, probability=0.02)
+Ci = Projection(pre=Pi, post=P, target='inh')
+Ci.connect_fixed_probability(weights=6.7, probability=0.02)
+
+compile()
+
+# ###########################################
+# Simulate
+# ###########################################
+P.start_record('spike')
+simulate(1000.0, measure_time=True)
+data = P.get_record()
+P.stop_record()
+
+# ###########################################
+# Make plots
+# ###########################################
+spikes = raster_plot(data['spike'])
+print 'Mean firing rate in the population:', len(spikes) / 4000., 'Hz'
+
+from pylab import *
+plot(dt*spikes[:, 0], spikes[:, 1], '.k', markersize=0.5)
+xlabel('Time (ms)')
+ylabel('# neuron')
+show()
---
+++ examples/vogels_abbott/CUBA.py
@@ -0,0 +1,69 @@
+from ANNarchy import *
+
+dt = 0.1
+setup(dt=dt)
+
+# ###########################################
+# Neuron model
+# ###########################################
+CUBA = Neuron(
+    parameters="""
+        El = -49.0      : population
+        Vr = -60.0      : population
+        Vt = -50.0      : population
+        tau_m = 20.0    : population
+        tau_exc = 5.0   : population
+        tau_inh = 10.0  : population
+    """,
+    equations="""
+        tau_m * dv/dt = (El - v) + g_exc + g_inh 
+
+        tau_exc * dg_exc/dt = - g_exc 
+        tau_inh * dg_inh/dt = - g_inh 
+    """,
+    spike = "v > Vt",
+    reset = "v = Vr",
+    refractory = 5.0
+)
+
+
+# ###########################################
+# Create Population
+# ###########################################
+P = Population(geometry=4000, neuron=CUBA)
+Pe = P[:3200]
+Pi = P[3200:]
+P.v = Uniform(-60.0, -50.0)
+
+# ###########################################
+# Connect the network
+# ###########################################
+we = 0.27 *60.0 / 10.0 # 0.7 * (Vmean - E_rev_exc) / gL (mV)
+wi = - 4.5 * 20.0 / 10.0 # 4.5 * (Vmean - E_rev_inh) / gL (mV)
+Ce = Projection(pre=Pe, post=P, target='exc')
+Ce.connect_fixed_probability(weights=we, probability=0.02)
+Ci = Projection(pre=Pi, post=P, target='inh')
+Ci.connect_fixed_probability(weights=wi, probability=0.02)
+
+compile()
+
+# ###########################################
+# Simulate
+# ###########################################
+P.start_record(['spike', 'v'])
+simulate(1000.0, measure_time=True)
+data = P.get_record()
+P.stop_record()
+
+
+###########################################
+# Make plots
+###########################################
+spikes = raster_plot(data['spike'])
+print 'Mean firing rate in the population:', len(spikes) / 4000., 'Hz'
+
+from pylab import *
+plot(dt*spikes[:, 0], spikes[:, 1], '.k', markersize=0.5)
+xlabel('Time (ms)')
+ylabel('# neuron')
+show()
