284e90a560a3384b042cac5ffbd2c29dd026b50e
Helge <helge.dinkelbach@gmail.com>
2015-05-25 12:18:36 +0200

    clarified output of access_test.

--- tests/access_test.py
+++ tests/access_test.py
@@ -14,47 +14,48 @@ print 'all element access'
 
 tmp = []
 for i in range(NTrials):
-    t1 = time.time()
+    t1 = time.clock()
     tmp = p1.r
-    t2 = time.time()
+    t2 = time.clock()
     acc_time += t2 - t1
 
 #print tmp, type(tmp)
-print (acc_time / NTrials) * 1000.0, 'ms'
+print 'old:',(acc_time / NTrials) * 1000.0, 'ms'
 
 acc_time = 0.0
 acc_time2 = 0.0
 for i in range(NTrials):
-    t1 = time.time()
+    t1 = time.clock()
     tmp = p1.cyInstance.get_r2()
-    t2 = time.time()
+    t2 = time.clock()
     acc_time += t2 - t1
-    t2 = time.time()
+    t2 = time.clock()
     tmp = np.array(tmp)
-    t3 = time.time()
+    t3 = time.clock()
     acc_time2 += t3 - t2
 
 #print tmp, type(tmp)
-print (acc_time / NTrials) * 1000.0, 'ms'
-print (acc_time2 / NTrials) * 1000.0, 'ms'
+print 'new:', (acc_time / NTrials) * 1000.0, 'ms'
+print 'conv:', (acc_time2 / NTrials) * 1000.0, 'ms'
 
 print 'single element access'
+acc_time = 0.0
 tmp = []
 for i in range(NTrials):
-    t1 = time.time()
+    t1 = time.clock()
     tmp = p1.cyInstance.get_single_r(10)
-    t2 = time.time()
+    t2 = time.clock()
     acc_time += t2 - t1
 
 print tmp, type(tmp)
-print (acc_time / NTrials) * 1000.0, 'ms'
+print 'old:', (acc_time / NTrials) * 1000.0 * 1000.0, 'us'
 
 acc_time = 0.0
 for i in range(NTrials):
-    t1 = time.time()
+    t1 = time.clock()
     tmp = p1.cyInstance.get_single_r2(10)
-    t2 = time.time()
+    t2 = time.clock()
 
 print tmp, type(tmp)
-print (acc_time / NTrials) * 1000.0, 'ms'
+print 'new:', (acc_time / NTrials) * 1000.0 * 1000.0, 'us'
 
