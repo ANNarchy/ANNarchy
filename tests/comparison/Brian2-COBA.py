c9ab75b2d26504fcb912508c10279c2c4c33c250
Julien Vitay <julien.vitay@informatik.tu-chemnitz.de>
2015-06-13 14:41:09 +0200

    comparison: Brian2 COBA benchmark uses a new measurmeent method.

--- tests/comparison/Brian2-COBA.py
+++ tests/comparison/Brian2-COBA.py
@@ -26,8 +26,8 @@ I = 20. * mvolt
 # ###########################################
 eqs = '''
 dv/dt  = (ge*(Erev_exc-v)+gi*(Erev_inh-v)-(v-El) + I)*(1./taum) : volt (unless refractory)
-dge/dt = -ge/taue : 1 (unless refractory)
-dgi/dt = -gi/taui : 1 (unless refractory)
+dge/dt = -ge/taue : 1 
+dgi/dt = -gi/taui : 1 
 '''
 
 # ###########################################
@@ -50,10 +50,16 @@ Ci.connect('i>=3200', p=0.02)
 # Simulation
 # ###########################################
 s_mon = SpikeMonitor(P)
-tstart = time.time()
+# Run for 0 second in order to measure compilation time
+t1 = time.time()
+run(0. * second)
+device.build(directory='COBA', compile=True, run=True, debug=False)
+t2 = time.time()
+# Run for 10 seconds
 run(10. * second)
 device.build(directory='COBA', compile=True, run=True, debug=False)
-print 'Done in', time.time()-tstart
+t3 = time.time()
+print 'Done in', t3 - 2*t2 + t1
 
 # ###########################################
 # Data analysis
