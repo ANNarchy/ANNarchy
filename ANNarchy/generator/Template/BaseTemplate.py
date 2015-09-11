ANNarchy/ANNarchy

commit e1d71dba64192b7c4141a3a5e8ed0bc7ded49edc

parallel_run() has now a same_seed argument to initialize the networks with the same seeds.
The seed (both for C++ and Numpy) can be changed with set_seed (globally or in a network).

ANNarchy/generator/Template/BaseTemplate.py
@@ -103,6 +103,12 @@ void setDt(double dt_);
 */
 void setNumberThreads(int threads);
 
+/*
+ * Seed for the RNG
+ *
+*/
+void setSeed(long int seed);
+
 #endif
 """
 
@@ -127,7 +133,7 @@ std::mt19937  rng;
 // Global operations
 %(glops_def)s
 
-// Recorders 
+// Recorders
 std::vector<Monitor*> recorders;
 void addRecorder(Monitor* recorder){
     recorders.push_back(recorder);
@@ -163,11 +169,21 @@ void initialize(double _dt, long int seed) {
 %(initialize)s
 }
 
+// Change the seed of the RNG
+void setSeed(long int seed){
+    if(seed==-1){
+        rng = std::mt19937(time(NULL));
+    }
+    else{
+        rng = std::mt19937(seed);
+    }
+}
+
 // Step method. Generated by ANNarchy.
 void step()
 {
 %(prof_step_pre)s
-    
+
     ////////////////////////////////
     // Presynaptic events
     ////////////////////////////////
@@ -197,7 +213,7 @@ void step()
     ////////////////////////////////
     // Update synaptic variables
     ////////////////////////////////
-%(update_synapse)s    
+%(update_synapse)s
 
     ////////////////////////////////
     // Postsynaptic events
@@ -248,7 +264,7 @@ omp_run_until_template = {
     run(steps);
     return steps;
 """,
-    'body': 
+    'body':
 """
     bool stop = false;
     int nb = 0;
@@ -281,12 +297,7 @@ omp_initialize_template = """
     t = (long int)(0);
 
     // Random number generators
-    if(seed==-1){
-        rng = std::mt19937(time(NULL));
-    }
-    else{
-        rng = std::mt19937(seed);
-    }
+    setSeed(seed);
 
     // Populations
 %(pop_init)s
@@ -447,6 +458,12 @@ void setTime(long int t_) ;
 double getDt() ;
 void setDt(double dt_);
 
+/*
+ * Seed for the RNG
+ *
+*/
+void setSeed(long int seed){printf("Setting seed not implemented on CUDA")};
+
 #endif
 """
 
@@ -495,7 +512,7 @@ __device__ __forceinline__ long modulo(long a, long b) { return a %% b; }
  * updating neural variables            *
  ****************************************/
 %(pop_kernel)s
- 
+
 /****************************************
  * weighted sum kernels                 *
  ****************************************/
@@ -548,7 +565,7 @@ double dt;
 long int t;
 long seed;
 
-// Recorders 
+// Recorders
 std::vector<Monitor*> recorders;
 void addRecorder(Monitor* recorder){
     recorders.push_back(recorder);
@@ -607,7 +624,7 @@ void run(int nbSteps) {
         single_step();
         //progress(i, nbSteps);
     }
-    
+
     //std::cout << std::endl;
 %(device_host_transfer)s
 }
@@ -642,7 +659,7 @@ void single_step()
     ////////////////////////////////
     // Update neural variables
     ////////////////////////////////
-%(update_neuron)s    
+%(update_neuron)s
 
     ////////////////////////////////
     // Delay outputs
@@ -659,7 +676,7 @@ void single_step()
     ////////////////////////////////
     // Update synaptic variables
     ////////////////////////////////
-%(update_synapse)s    
+%(update_synapse)s
 
     ////////////////////////////////
     // Postsynaptic events
@@ -671,7 +688,7 @@ void single_step()
     for(int i=0; i < recorders.size(); i++){
         recorders[i]->record();
     }
-    
+
     ////////////////////////////////
     // Increase internal time
     ////////////////////////////////

ANNarchy/core/Global.py
@@ -1,9 +1,9 @@
 """
 
     Global.py
-    
+
     This file is part of ANNarchy.
-    
+
     Copyright (C) 2013-2016  Julien Vitay <julien.vitay@gmail.com>,
     Helge Uelo Dinkelbach <helge.dinkelbach@gmail.com>
 
@@ -20,7 +20,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""    
+"""
 import sys, os
 import time
 from math import ceil
@@ -30,7 +30,7 @@ import numpy as np
 _objects = {
     'functions': [],
     'neurons': [],
-    'synapses': [],    
+    'synapses': [],
 }
 
 # Data for the different networks
@@ -46,7 +46,7 @@ _network = [
 
 # Configuration
 config = dict(
-   { 
+   {
     'dt' : 1.0,
     'verbose': False,
     'show_time': False,
@@ -73,8 +73,8 @@ OMP_MIN_NB_NEURONS = 100
 # Authorized keywork for attributes
 authorized_keywords = [
     # Init
-    'init',      
-    # Bounds             
+    'init',
+    # Bounds
     'min',
     'max',
     # Locality
@@ -101,31 +101,31 @@ _recorded_populations = {}
 
 def setup(**keyValueArgs):
     """
-    The setup function is used to configure ANNarchy simulation environment. It takes various optional arguments: 
+    The setup function is used to configure ANNarchy simulation environment. It takes various optional arguments:
 
     *Parameters*:
-    
+
     * **dt**: simulation step size (default: 1.0 ms).
 
     * **paradigm**: parallel framework for code generation. Accepted values: "openmp" or "cuda" (default: "openmp").
 
     * **method**: default method to numerize ODEs. Default is the explicit forward Euler method ('explicit').
-    
+
     * **num_threads**: number of treads used by openMP (overrides the environment variable ``OMP_NUM_THREADS`` when set, default = None).
 
     * **structural_plasticity**: allows synapses to be dynamically added/removed during the simulation (default: False).
 
-    * **seed**: the seed (integer) to be used in the random number generators (default = -1 is equivalent to time(NULL)). 
-    
+    * **seed**: the seed (integer) to be used in the random number generators (default = -1 is equivalent to time(NULL)).
+
     The following parameters are mainly for debugging and profiling, and should be ignored by most users:
-    
+
     * **verbose**: shows details about compilation process on console (by default False). Additional some information of the network construction will be shown.
-    
+
     * **suppress_warnings**: if True, warnings (e. g. from the mathematical parser) are suppressed.
-    
+
     * **show_time**: if True, initialization times are shown. Attention: verbose should be set to True additionally.
-    
-    
+
+
     .. note::
 
         This function should be used before any other functions of ANNarchy, right after ``from ANNarchy import *``::
@@ -144,7 +144,7 @@ def setup(**keyValueArgs):
         if key == 'seed':
             np.random.seed(keyValueArgs[key])
 
-    
+
 def reset(populations=True, projections=False, synapses = False, net_id=0):
     """
     Reinitialises the network to its state before the call to compile.
@@ -158,32 +158,32 @@ def reset(populations=True, projections=False, synapses = False, net_id=0):
     if populations:
         for pop in _network[net_id]['populations']:
             pop.reset()
-            
+
     if projections:
         for proj in _network[net_id]['projections']:
             proj.reset(synapses)
 
     _network[net_id]['instance'].set_time(0)
-        
+
 def get_population(name, net_id=0):
     """
     Returns the population with the given *name*.
-    
+
     *Parameter*:
-    
+
     * **name**: name of the population
 
     Returns:
-    
+
     * The requested ``Population`` object if existing, ``None`` otherwise.
     """
     for pop in _network[net_id]['populations']:
         if pop.name == name:
             return pop
-        
+
     _error("get_population(): the population", name, "does not exist.")
     return None
-    
+
 def populations(net_id=0):
     """
     Returns a list of all declared populations.
@@ -199,13 +199,13 @@ def projections(net_id=0):
 def add_function(function):
     """
     Defines a global function which can be used by all neurons and synapses.
-    
+
     The function must have only one return value and use only the passed arguments.
-    
+
     Examples of valid functions:
-    
+
         logistic(x) = 1 / (1 + exp(-x))
-        
+
         piecewise(x, a, b) =    if x < a:
                                     a
                                 else:
@@ -213,11 +213,11 @@ def add_function(function):
                                         b
                                     else:
                                         x
-    
+
     Please refer to the manual to know the allowed mathematical functions.
-    """  
+    """
     _objects['functions'].append(function)
-    
+
 def simulate(duration, measure_time = False, net_id=0):
     """
     Runs the network for the given duration in milliseconds. The number of simulation steps is  computed relative to the discretization step ``dt`` declared in ``setup()`` (default: 1ms)::
@@ -231,16 +231,16 @@ def simulate(duration, measure_time = False, net_id=0):
     """
     nb_steps = ceil(float(duration) / config['dt'])
 
-    if _network[net_id]['instance']:      
+    if _network[net_id]['instance']:
         if measure_time:
-            tstart = time.time() 
+            tstart = time.time()
         _network[net_id]['instance'].pyx_run(nb_steps)
         if measure_time:
             _print('Simulating', duration/1000.0, 'seconds of the network took', time.time() - tstart, 'seconds.')
     else:
         _error('simulate(): the network is not compiled yet.')
         return
-    
+
 def simulate_until(max_duration, population, operator='and', measure_time = False, net_id=0):
     """
     Runs the network for the maximal duration in milliseconds. If the ``stop_condition`` defined in the population becomes true during the simulation, it is stopped.
@@ -267,9 +267,9 @@ def simulate_until(max_duration, population, operator='and', measure_time = Fals
     nb_steps = ceil(float(max_duration) / config['dt'])
     if not isinstance(population, list):
         population = [population]
-    if _network[net_id]['instance']:      
+    if _network[net_id]['instance']:
         if measure_time:
-            tstart = time.time() 
+            tstart = time.time()
         nb = _network[net_id]['instance'].pyx_run_until(nb_steps, [pop.id for pop in population], True if operator=='and' else False)
         sim_time = float(nb) / config['dt']
         if measure_time:
@@ -281,9 +281,9 @@ def simulate_until(max_duration, population, operator='and', measure_time = Fals
 
 def step(net_id=0):
     """
-    Performs a single simulation step (duration = ``dt``). 
+    Performs a single simulation step (duration = ``dt``).
     """
-    if _network[net_id]['instance']:      
+    if _network[net_id]['instance']:
         _network[net_id]['instance'].pyx_step()
     else:
         _error('simulate(): the network is not compiled yet.')
@@ -295,29 +295,29 @@ def step(net_id=0):
 def enable_learning(projections=None, net_id=0):
     """
     Enables learning for all projections.
-    
+
     *Parameter*:
-    
+
     * **projections**: the projections whose learning should be enabled. By default, all the existing projections are enabled.
     """
     if not projections:
         projections = _network[net_id]['projections']
     for proj in projections:
         proj.enable_learning()
-        
+
 def disable_learning(projections=None, net_id=0):
     """
     Disables learning for all projections.
-    
+
     *Parameter*:
-    
+
     * **projections**: the projections whose learning should be disabled. By default, all the existing projections are disabled.
     """
     if not projections:
         projections = _network[net_id]['projections']
     for proj in projections:
         proj.disable_learning()
-    
+
 ################################
 ## Time
 ################################
@@ -366,23 +366,38 @@ def dt():
     return config['dt']
 
 ################################
+## Seed
+################################
+def set_seed(seed, net_id=0):
+    "Sets the seed of the random number generators, both in numpy.random and in the C++ library when it is created."
+    config['seed'] = seed
+    if seed > -1:
+        np.random.seed(seed)
+    try:
+        _network[net_id]['instance'].set_seed(seed)
+    except:
+        _warning('The seed will only be set in the simulated network when it is compiled.')
+
+
+
+################################
 ## Recording
 ################################
 def start_record(to_record, period = {}):
     """
-    Starts recording of variables in different populations. 
-    
+    Starts recording of variables in different populations.
+
     *Parameter*:
-    
-    * **to_record**: a dictionary with population objects (or names) as keys and variable names as values (either a single string or a list of strings). 
+
+    * **to_record**: a dictionary with population objects (or names) as keys and variable names as values (either a single string or a list of strings).
 
     * **period**: a dictionary with population objects as keys and record periods (in ms) as values (default: dt for each).
 
     Example::
-    
-        to_record = { 
-            pop1 : ['mp', 'r'], 
-            pop2 : 'mp'    
+
+        to_record = {
+            pop1 : ['mp', 'r'],
+            pop2 : 'mp'
         }
         start_record(to_record)
     """
@@ -408,35 +423,35 @@ def start_record(to_record, period = {}):
 
 def get_record(to_record=None, reshape=False):
     """
-    Retrieves the recorded variables of one or more populations since the last call. 
-  
+    Retrieves the recorded variables of one or more populations since the last call.
+
     *Parameter*:
-    
+
     * **to_record**: a dictionary containing population objects (or names) as keys and variable names as values. For more details check Population.start_record(). When omitted, the dictionary provided in the last call to start_record() is used.
 
     * **reshape**: defines if the recorded variables should be reshaped to match the population geometry (default: False).
-    
+
     Returns:
-    
+
     * A dictionary containing all recorded values. The dictionary is empty if no recorded data is available.
-    
+
     Example::
-    
-        to_record = { 
-            pop1 : ['mp', 'r'], 
-            pop2: 'mp'    
+
+        to_record = {
+            pop1 : ['mp', 'r'],
+            pop2: 'mp'
         }
         start_record(to_record)
         simulate(1000.0)
         data = get_record()
-        
-    """   
+
+    """
     _warning('Recording through global methods is deprecated. Use the Monitor object instead.')
     if not to_record:
         to_record = _recorded_populations
 
     data = {}
-    
+
     for pop, variables in to_record.items():
         if not isinstance(pop, str):
             pop_object = pop
@@ -444,16 +459,16 @@ def get_record(to_record=None, reshape=False):
             pop_object = get_population(pop)
 
         data[pop] = pop_object.get_record(variables, reshape)
-    
-    return data  
 
-        
+    return data
+
+
 def stop_record(to_record=None):
     """
-    Stops the recording of variables in different populations. 
-    
+    Stops the recording of variables in different populations.
+
     *Parameter*:
-    
+
     * **to_record**: a dictionary with population objects (or names) as keys and variable names as values (either a single string or a list of strings). For more details check Population.stop_record(). When omitted, the dictionary provided in the last call to start_record() is used.
     """
     _warning('Recording through global methods is deprecated. Use the Monitor object instead.')
@@ -465,13 +480,13 @@ def stop_record(to_record=None):
         else:
             get_population(pop).stop_record(variables)
 
-        
+
 def pause_record(to_record=None):
     """
-    Pauses the recording of variables in different populations. 
-    
+    Pauses the recording of variables in different populations.
+
     *Parameter*:
-    
+
     * **to_record**: a dictionary with population objects (or names) as keys and variable names as values (either a single string or a list of strings). For more details check Population.pause_record(). When omitted, the dictionary provided in the last call to start_record() is used.
     """
     _warning('Recording through global methods is deprecated. Use the Monitor object instead.')
@@ -483,13 +498,13 @@ def pause_record(to_record=None):
         else:
             get_population(pop).pause_record(variables)
 
-        
+
 def resume_record(to_record=None):
     """
-    Resumes the recording of variables in different populations. 
-    
+    Resumes the recording of variables in different populations.
+
     *Parameter*:
-    
+
     * **to_record**: a dictionary with population objects (or names) as keys and variable names as values (either a single string or a list of strings). For more details check Population.resume_record(). When omitted, the dictionary provided in the last call to start_record() is used.
     """
     _warning('Recording through global methods is deprecated. Use the Monitor object instead.')
@@ -508,7 +523,7 @@ def resume_record(to_record=None):
 def _print(*var_text):
     """
     Prints a message to standard out.
-    """    
+    """
     text = ''
     for var in var_text:
         text += str(var) + ' '
@@ -517,15 +532,15 @@ def _print(*var_text):
 def _debug(*var_text):
     """
     Prints a message to standard out, if verbose mode set True.
-    """    
+    """
     if not config['verbose']:
         return
-    
+
     text = ''
     for var in var_text:
         text += str(var) + ' '
     print(text)
-        
+
 def _warning(*var_text):
     """
     Prints a warning message to standard out.
@@ -535,13 +550,12 @@ def _warning(*var_text):
         text += str(var) + ' '
     if not config['suppress_warnings']:
         print(text)
-        
+
 def _error(*var_text):
     """
     Prints an error message to standard out.
     """
     text = 'ERROR: '
     for var in var_text:
-        text += str(var) + ' '    
+        text += str(var) + ' '
     print(text)
-

ANNarchy/core/Network.py
@@ -9,7 +9,7 @@ import os, shutil, sys
 import numpy as np
 
 class Network(object):
-    """ 
+    """
     A network gathers already defined populations, projections and monitors in order to run them independently.
 
     This is particularly useful when varying single parameters of a network and comparing the results.
@@ -45,7 +45,7 @@ class Network(object):
         """
         *Parameters:*
 
-        * **everything**: defines if all existing populations and projections should be automatically added (default: False). 
+        * **everything**: defines if all existing populations and projections should be automatically added (default: False).
         """
         self.id = len(Global._network)
         self.everything = everything
@@ -71,7 +71,7 @@ class Network(object):
         Adds a Population, Projection or Monitor to the network.
 
         *Parameters:*
-        
+
         * **objects**: A single object or a list to add to the network.
         """
         if isinstance(objects, list):
@@ -126,7 +126,7 @@ class Network(object):
             # Add the copy to the local network
             Global._network[self.id]['projections'].append(proj)
             self.projections.append(proj)
-            
+
         elif isinstance(obj, Monitor):
             m = Monitor(obj.object, variables=obj.variables, period=obj._period, start=obj._start, net_id=self.id)
             # Add the copy to the local network (the monitor writes itself already in the right network)
@@ -187,7 +187,7 @@ class Network(object):
             Global.simulate(duration, net_id=self.id)
         """
         Global.simulate(duration, measure_time, self.id)
-    
+
     def simulate_until(self, max_duration, population, operator='and', measure_time = False):
         """
         Runs the network for the maximal duration in milliseconds. If the ``stop_condition`` defined in the population becomes true during the simulation, it is stopped.
@@ -212,10 +212,10 @@ class Network(object):
         * the actual duration of the simulation in milliseconds.
         """
         return Global.simulate_until(max_duration, population, operator, measure_time, self.id)
-    
+
     def step(self):
         """
-        Performs a single simulation step (duration = ``dt``). 
+        Performs a single simulation step (duration = ``dt``).
         """
         Global.step(self.id)
 
@@ -232,7 +232,7 @@ class Network(object):
         """
         Global.reset(populations,  projections, synapses, self.id)
 
-    def get_time(self, net_id=0):
+    def get_time(self):
         "Returns the current time in ms."
         return Global.get_time(self.id)
 
@@ -245,11 +245,11 @@ class Network(object):
         """
         Global.set_time(t, self.id)
 
-    def get_current_step(self, net_id=0):
+    def get_current_step(self):
         "Returns the current simulation step."
         return Global.get_current_step(self.id)
 
-    def set_current_step(self, t, net_id=0):
+    def set_current_step(self, t):
         """Sets the current simulation step.
 
         .. warning::
@@ -258,12 +258,18 @@ class Network(object):
         """
         Global.set_current_step(t, self.id)
 
+    def set_seed(self, seed):
+        """
+        Sets the seed of the random number generators for this network.
+        """
+        Global.set_seed(seed, self.id)
+
     def enable_learning(self, projections=None):
         """
         Enables learning for all projections.
-        
+
         *Parameter*:
-        
+
         * **projections**: the projections whose learning should be enabled. By default, all the existing projections are disabled.
         """
         if not projections:
@@ -274,9 +280,9 @@ class Network(object):
     def disable_learning(self, projections=None):
         """
         Disables learning for all projections.
-        
+
         *Parameter*:
-        
+
         * **projections**: the projections whose learning should be disabled. By default, all the existing projections are disabled.
         """
         if not projections:
@@ -285,13 +291,13 @@ class Network(object):
             proj.disable_learning()
 
 
-def parallel_run(method, networks=None, number=0, max_processes=-1, measure_time=False, sequential=False):
+def parallel_run(method, networks=None, number=0, max_processes=-1, measure_time=False, sequential=False, same_seed=False):
     """
     Allows to run multiple networks in parallel using multiprocessing.
 
     If the ``networks`` argument is provided as a list of Network objects, the given method will be executed for each of these networks.
 
-    If ``number`` is given instead, the same number of networks will be created and the method is applied. 
+    If ``number`` is given instead, the same number of networks will be created and the method is applied.
 
     **Returns**:
 
@@ -304,8 +310,9 @@ def parallel_run(method, networks=None, number=0, max_processes=-1, measure_time
     * **method**: a Python method which will be executed for each network. This function must accept an integer as first argument (id of the simulation) and a Network object as second argument.
     * **networks**: a list of networks to simulate in parallel.
     * **max_processes**: maximal number of processes to start concurrently (default: the available number of cores on the machine).
-    * **measure_time**: if the total simulation time should be printed out. 
-    * **sequential**: if True, runs the simulations sequentially instead of in parallel (default: False). 
+    * **measure_time**: if the total simulation time should be printed out.
+    * **sequential**: if True, runs the simulations sequentially instead of in parallel (default: False).
+    * **same_seed**: if True, all networks will use the same seed. If not, the seed will be randomly initialized with time(0) for each network (default).
     """
     # Check inputs
     if not networks and number < 1:
@@ -318,21 +325,21 @@ def parallel_run(method, networks=None, number=0, max_processes=-1, measure_time
         return []
 
     if not networks: # The magic network will run N times
-        return _parallel_multi(method, number, max_processes, measure_time, sequential)
+        return _parallel_multi(method, number, max_processes, measure_time, sequential, same_seed)
 
     if not isinstance(networks, list):
         Global._error('parallel_run(): the networks argument must be a list.')
         return []
 
     # Simulate the different networks
-    return _parallel_networks(method, networks, max_processes, measure_time, sequential)
+    return _parallel_networks(method, networks, max_processes, measure_time, sequential, same_seed)
 
-def _parallel_networks(method, networks, max_processes, measure_time, sequential):
+def _parallel_networks(method, networks, max_processes, measure_time, sequential, same_seed):
     " Method when different networks are provided"
     import multiprocessing
     from multiprocessing.dummy import Pool
 
-    # Time measurement    
+    # Time measurement
     from time import time
     if measure_time:
         ts = time()
@@ -375,12 +382,12 @@ def _parallel_networks(method, networks, max_processes, measure_time, sequential
     return results
 
 
-def _parallel_multi(method, number, max_processes, measure_time, sequential):
+def _parallel_multi(method, number, max_processes, measure_time, sequential, same_seed):
     "Method when the same network must be simulated multiple times."
     import multiprocessing
     from multiprocessing import Pool
 
-    # Time measurement    
+    # Time measurement
     from time import time
     if measure_time:
         ts = time()
@@ -389,11 +396,19 @@ def _parallel_multi(method, number, max_processes, measure_time, sequential):
     if max_processes < 0:
         max_processes = min(number, multiprocessing.cpu_count())
 
+    # Seed
+    if same_seed and Global.config['seed'] > -1: # use the global seed
+        seed =  Global.config['seed']
+    elif same_seed and Global.config['seed'] == -1: # not defined, but should be the same for all networks
+        seed = np.random.get_state()[1][0] # not the current seed, but close enough...
+    else: # draw it everytime with time(0)
+        seed = -1
+
     # Simulation
     if not sequential:
         try:
             pool = Pool(max_processes)
-            results = pool.map(_create_and_run_method, [(n, method) for n in range(number)] )
+            results = pool.map(_create_and_run_method, [(n, method, seed) for n in range(number)] )
             pool.close()
             pool.join()
         except Exception as e:
@@ -404,7 +419,7 @@ def _parallel_multi(method, number, max_processes, measure_time, sequential):
         results = []
         try:
             for n in range(number):
-                results.append(_create_and_run_method((n, method)))
+                results.append(_create_and_run_method((n, method, seed)))
         except Exception as e:
             Global._print(e)
             Global._error('parallel_run(): running ' + str(number) + ' networks failed.')
@@ -419,15 +434,19 @@ def _parallel_multi(method, number, max_processes, measure_time, sequential):
             msg += ' sequentially '
         msg += 'took: ' + str(time()-ts)
         Global._print(msg)
-        
+
     return results
 
 def _create_and_run_method(args):
     "method called to wrap the user-defined method"
-    n, method = args
+    n, method, seed = args
     net = Network(True)
-    np.random.seed() # TODO: if seed is declared
     Compiler._instantiate(net.id, 0)
+    if seed == -1:
+        net.set_seed(seed)
+        np.random.seed() # Would set -1 otherwise
+    else:
+        net.set_seed(seed)
     res = method(n, net)
     del net
     return res

ANNarchy/generator/Template/PyxTemplate.py
@@ -31,10 +31,11 @@ cdef extern from "ANNarchy.h":
 
     # Methods
     void initialize(double, long)
+    void setSeed(long)
     void run(int nbSteps) nogil
     int run_until(int steps, vector[int] populations, bool or_and)
     void step()
-    
+
     # Time
     long getTime()
     void setTime(long)
@@ -120,6 +121,10 @@ def get_dt():
 # Set number of threads
 def set_number_threads(int n):
     setNumberThreads(n)
+
+# Set seed
+def set_seed(long seed):
+    setSeed(seed)
 '''
 
 # Export for populations

commit 2eaabf31cbfdc44708db6bf6dd6cab91046f7119

+ Transmission/Update flag according commit 0b60628 + Fixed compiler error on CUDA introduced by commit e1d71db
*TODO*:
- initialize seeds due setSeed(long) )
- plasticity flag

ANNarchy/generator/Template/BaseTemplate.py
@@ -461,8 +461,8 @@ void setDt(double dt_);
 /*
  * Seed for the RNG
  *
-*/
-void setSeed(long int seed){printf("Setting seed not implemented on CUDA")};
+ */
+inline void setSeed(long int seed){ printf("Setting seed not implemented on CUDA"); }
 
 #endif
 """

ANNarchy/generator/Template/ProjectionTemplate.py
@@ -1165,7 +1165,7 @@ __global__ void cuProj%(id)s_step( /* default params */
 cuda_synapse_kernel_call =\
 """
     // proj%(id_proj)s: pop%(pre)s -> pop%(post)s
-    if ( pop%(post)s._active && proj%(id_proj)s._plasticity ) {
+    if ( proj%(id_proj)s._transmission && proj%(id_proj)s._update && proj%(id_proj)s._plasticity ) {
         cuProj%(id_proj)s_step<<< pop1.size, __pop%(pre)s_pop%(post)s_%(target)s__, 0, proj%(id_proj)s.stream>>>(
             proj%(id_proj)s.gpu_post_rank,
             proj%(id_proj)s.gpu_pre_rank,