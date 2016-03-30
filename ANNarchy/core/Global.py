acf1f0013dfbfe01a1463e115254ae6380896853
Julien Vitay <julien.vitay@informatik.tu-chemnitz.de>
2016-03-30 14:08:24 +0200

    every decorator implemented, still a separate simulate() method.

--- ANNarchy/core/Global.py
+++ ANNarchy/core/Global.py
@@ -231,17 +231,21 @@ def simulate(duration, measure_time = False, net_id=0):
     * **duration**: the duration in milliseconds.
     * **measure_time**: defines whether the simulation time should be printed (default=False).
     """
+    if not _network[net_id]['instance']:
+        _error('simulate(): the network is not compiled yet.')
+        exit(0)
+
+    # Compute the number of steps
     nb_steps = ceil(float(duration) / config['dt'])
 
-    if _network[net_id]['instance']:
-        if measure_time:
-            tstart = time.time()
-        _network[net_id]['instance'].pyx_run(nb_steps)
-        if measure_time:
-            _print('Simulating', duration/1000.0, 'seconds of the network took', time.time() - tstart, 'seconds.')
-    else:
-        _error('simulate(): the network is not compiled yet.')
-        return
+    if measure_time:
+        tstart = time.time()
+
+    _network[net_id]['instance'].pyx_run(nb_steps)
+
+    if measure_time:
+        _print('Simulating', duration/1000.0, 'seconds of the network took', time.time() - tstart, 'seconds.')
+
 
 def simulate_until(max_duration, population, operator='and', measure_time = False, net_id=0):
     """
@@ -266,20 +270,25 @@ def simulate_until(max_duration, population, operator='and', measure_time = Fals
 
     * the actual duration of the simulation in milliseconds.
     """
+    if not _network[net_id]['instance']:
+        _error('simulate_until(): the network is not compiled yet.')
+        exit(0)
+
     nb_steps = ceil(float(max_duration) / config['dt'])
     if not isinstance(population, list):
         population = [population]
-    if _network[net_id]['instance']:
-        if measure_time:
-            tstart = time.time()
-        nb = _network[net_id]['instance'].pyx_run_until(nb_steps, [pop.id for pop in population], True if operator=='and' else False)
-        sim_time = float(nb) / config['dt']
-        if measure_time:
-            _print('Simulating', nb/config['dt']/1000.0, 'seconds of the network took', time.time() - tstart, 'seconds.')
-        return sim_time
-    else:
-        _error('simulate(): the network is not compiled yet.')
-        return 0.0
+
+
+    if measure_time:
+        tstart = time.time()
+
+    nb = _network[net_id]['instance'].pyx_run_until(nb_steps, [pop.id for pop in population], True if operator=='and' else False)
+    
+    sim_time = float(nb) / config['dt']
+    if measure_time:
+        _print('Simulating', nb/config['dt']/1000.0, 'seconds of the network took', time.time() - tstart, 'seconds.')
+    return sim_time
+
 
 def step(net_id=0):
     """
@@ -570,18 +579,56 @@ def _error(*var_text, **args):
 ################################
 ## Decorators
 ################################
-
 _callbacks = []
+_callbacks_enabled = True
+
+def callbacks_enabled():
+    return _callbacks_enabled
+
+def disable_callbacks():
+    global _callbacks_enabled
+    _callbacks_enabled = False
+
+def enable_callbacks():
+    global _callbacks_enabled
+    _callbacks_enabled = True
 
 class every(object):
+    """
+    Decorator to declare a callback method that will be called periodically during the simulation.
 
-    def __init__(self, period, offset=0.):
+    Example of setting increasing inputs to a population every 100 ms, with an offset of 90 ms (or -10 ms relative to the period)::
+
+        @every(period=100., offset=-10.)
+        def step_input(n):
+            pop.I = float(n) / 100.
+
+        simulate(10000.)
+
+    ``step_input()`` will be called at times 90., 190., ..., 9990. ms during the call to ``simulate()``.
+
+    The method must accept only ``n`` as parameter (an integer being 0 the first time the method is called, and incremented afterwards) and can not return anything.
+    
+    The times at which the method is called are relative to the time when ``simulate()`` is called.
+
+    If multiple callbacks are defined, they will be called in the order of their declaration if they occur at the same time.
+
+    """
+
+    def __init__(self, period, offset=0., wait=0.0):
         """
-        If there are decorator arguments, the function
-        to be decorated is not passed to the constructor!
+        *Parameters:*
+
+        * **period**: interval in ms between two calls to the function.
+        * **offset**: by default, the first call to the method will be made at the start of the simulation. The offset delays the call within the period (default: 0.0). Can be negative, in which case it will be counted from the end of the period.
+        * **wait**: allows to wait for a certain amount of time (in ms) before starting to call the method. 
+
+        ``wait`` can be combined with ``offset``, so if ``period=100.``, ``offset=50.`` and ``wait=500.``, the first call will be made 500 ms after the call to ``simulate()``
+
         """
-        self.period = period
-        self.offset = offset
+        self.period = float(period)
+        self.offset = float(offset)
+        self.wait = float(wait)
         _callbacks.append(self)
 
     def __call__(self, f):
@@ -593,5 +640,46 @@ class every(object):
         self.func = f
         return f
 
-def list_callbacks():
-    return _callbacks
+
+def simulate_with_callbacks(duration, net_id=0):
+    """
+    Tmp: replaces simulate when call_backs are defined.
+    """
+    if not _network[net_id]['instance']:
+        _error('simulate(): the network is not compiled yet.')
+        exit(0)
+
+    if not callbacks_enabled():
+        return simulate(duration)
+        
+    t_start = get_current_step()
+    length = int(duration*dt())
+
+    # Compute the times
+    times = []
+    for c in _callbacks:
+        period = int(c.period*dt())
+        offset = int(c.offset*dt()) % period
+        wait = int(c.wait*dt())
+
+        moments = range(t_start + wait + offset, t_start + length, period)
+        n = 0
+        for m in moments:
+            times.append((m, c, n))
+            n += 1
+
+    # Sort the times to be surethey are in the right order.
+    # TODO: make sure the callbacks stay in the declaration order.
+    times = sorted(times)
+
+    for time, callback, n in times:
+        print time, 
+        # Advance the simulation to the desired time
+        if time != get_current_step():
+            _network[net_id]['instance'].pyx_run(time-get_current_step())
+        # Call the callback
+        callback.func(n)
+
+    # Go to the end of the duration
+    if get_current_step() < t_start + length:
+        _network[net_id]['instance'].pyx_run(t_start + length - get_current_step())
