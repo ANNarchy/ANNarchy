298486d256b08342703eac6481282cd80f578d56
Helge Dinkelbach <helge.dinkelbach@gmail.com>
2016-08-17 14:19:10 +0200

    missing files.

---
+++ ANNarchy/core/cython_ext/CSRMatrix.hpp
@@ -0,0 +1,135 @@
+/*
+ *  Copyright (C) 2016-2018 Helge Dinkelbach <helge.dinkelbach@gmail.com>
+ *
+ *  CSRMatrix.hpp is part of ANNarchy
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Foobar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this headers. If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+#include <iostream>
+#include <random>
+#include <iomanip>
+
+class CSRMatrix {
+	const int num_rows_;					///< number of rows
+	const int num_columns_;				///< number of columns
+
+	std::vector<int> fwd_row_;		        ///< i-th row in the matrix ranges from fwd_row_[i] to fwd_row_[i+1]
+	std::vector<int> fwd_col_idx_;         ///< accessing with fwd_col_idx_[fwd_row_[i]] to fwd_col_idx_[fwd_row_[i+1]] provides acces to all column indices
+	std::vector<double> values_;
+
+public:
+	CSRMatrix(const int num_rows, const int num_columns) :
+		num_rows_(num_rows+1), num_columns_(num_columns+1) {
+
+		fwd_row_ = std::vector<int>(num_rows_, 0);
+		fwd_col_idx_ = std::vector<int>();
+		values_ = std::vector<double>();
+	}
+
+	~CSRMatrix() {
+		// TODO:
+	}
+
+	inline size_t size_in_bytes() {
+		size_t size = 0;
+
+		// TODO:
+		return size;
+	}
+
+	inline int num_rows() {
+		return num_rows_-1;
+	}
+
+	inline int num_columns() {
+		return num_columns_-1;
+	}
+
+	/**
+	 *	Forward View
+	 */
+	inline std::vector<int> row_begin() {
+		return fwd_row_;
+	}
+
+	inline std::vector<int> column_indices() {
+		return fwd_col_idx_;
+	}
+
+	inline std::vector<double> values() {
+		return values_;
+	}
+
+	/**
+	 * 	\brief		add all connections of one dendrite
+	 * 	\details
+	 * 	\param[in]	row			neuron index of afferent neuron
+	 * 	\param[in]	columns		neuron indices of efferent neurons
+	 * 	\param[in]	w			synaptic weights
+	 */
+	void push_back(int row, std::vector<int> columns, std::vector<double> w, std::vector<int> d) {
+		int old_idx = fwd_row_[row+1];
+
+		fwd_col_idx_.insert(fwd_col_idx_.begin()+old_idx, columns.begin(), columns.end());
+		values_.insert(values_.begin()+old_idx, w.begin(), w.end());
+
+		for( auto it = fwd_row_.begin()+row+1; it != fwd_row_.end(); it++ )
+			*it += columns.size();
+	}
+
+	/**
+	 * 	\brief		overloaded std::ostream operator<<
+	 * 	\details	for the object itself
+	 * 	\param[IN]	os		ostream instance
+	 * 	\param[IN]	matrix	object instance
+	 * 	\return		manipulated ostream instance
+	 */
+	friend std::ostream& operator<< (std::ostream& os, const CSRMatrix& matrix) {
+        os << "(forward)" << std::endl;
+		os << "fwd_row_:" << std::endl;
+		os << "[ ";
+		for(int r = 0; r < matrix.num_rows_; r++) {
+			os << matrix.fwd_row_[r] << " ";
+		}
+		os << "]" << std::endl;
+
+		os << "fwd_col_idx:" << std::endl;
+		os << "[ ";
+		for(unsigned int idx = 0; idx < matrix.values_.size(); idx++) {
+			os << matrix.fwd_col_idx_[idx] << " ";
+		}
+		os << "]" << std::endl;
+
+		os << "values:" << std::endl;
+		os << "[ ";
+		for(unsigned int idx = 0; idx < matrix.values_.size(); idx++) {
+			os << std::setprecision(2) << matrix.values_[idx] << " ";
+		}
+		os << "]" << std::endl;
+
+		return os;
+	}
+
+	/**
+	 * 	\brief		overloaded std::ostream operator<<
+	 * 	\details	for the reference to an object
+	 * 	\param[IN]	os		ostream instance
+	 * 	\param[IN]	matrix	object reference
+	 * 	\return		manipulated ostream instance
+	 */
+	friend std::ostream& operator<< (std::ostream& os, CSRMatrix* matrix) {
+		return os << *matrix;
+	}
+};
---
+++ ANNarchy/generator/Projection/Connectivity/CSR_OpenMP.py
@@ -0,0 +1,206 @@
+######################################
+### CSRC Connectivity matrix
+######################################
+connectivity_matrix = {
+    'declare': """
+    std::vector<int> _row_ptr;
+    std::vector<int> _post_ranks;
+""",
+    'accessor': """
+    // Accessor to connectivity data
+    std::vector<int> get_post_rank() {
+        // TODO: maybe throw an exception here
+        if ( _row_ptr.empty() )
+            return std::vector<int>();
+
+        auto post_ranks = std::vector<int>();
+        for( int i = 0; i < _row_ptr.size()-1; i++ )
+            if ( _row_ptr[i] != _row_ptr[i+1] )
+                post_ranks.push_back(i);
+        return post_ranks;
+    }
+    
+    int nb_synapses(int n) { return _post_ranks.size(); }
+    
+    // CSR specific
+    void set_row_ptr(std::vector<int> row_ptr) { _row_ptr = row_ptr; }
+    void set_post_ranks(std::vector<int> post_ranks) { _post_ranks = post_ranks; }
+""",
+    'init': """
+""",
+    'pyx_struct': """
+        # LIL Connectivity
+        vector[int] get_post_rank()
+        
+        # CSR Connectivity
+        void set_row_ptr(vector[int])
+        void set_post_ranks(vector[int])
+""",
+    'pyx_wrapper_args': "synapses",
+    'pyx_wrapper_init': """
+        cdef CSR syn = synapses
+
+        proj%(id_proj)s.set_row_ptr(syn._matrix.row_begin())
+        proj%(id_proj)s.set_post_ranks(syn._matrix.column_indices())
+""",
+    'pyx_wrapper_accessor': """
+    # Connectivity
+    def post_rank(self):
+        return proj%(id_proj)s.get_post_rank()
+    def set_post_rank(self, val):
+        # makes no sense, as the row_idx cannot be initialized sensefully 
+        pass
+"""
+}
+
+weight_matrix = {
+    'declare': """
+    std::vector<double> w;
+""",
+    'accessor': """
+    void set_w_csr(std::vector<double> w) { this->w = w; }
+""",
+    'init': """
+""",
+    'pyx_struct': """
+        void set_w_csr(vector[double])
+""",
+    'pyx_wrapper_args': "",
+    'pyx_wrapper_init': """
+        proj%(id_proj)s.set_w_csr(syn._matrix.values())
+""",
+    'pyx_wrapper_accessor': """
+"""
+}
+
+single_weight_matrix = {
+    'declare': """
+    // Single weight in the projection
+    // TODO:
+""",
+    'accessor': "",
+    'init': "",
+    'pyx_struct': """
+        # Local variable w
+        # TODO:
+""",
+    'pyx_wrapper_args': "",
+    'pyx_wrapper_init': """
+        # Use only the first weight
+        # TODO:
+""",
+    'pyx_wrapper_accessor': """
+    # Local variable w
+    # TODO:
+"""
+}
+# These fields can remain empty as the CSRC already
+# contains the backward view
+inverse_connectivity_matrix = {
+    'declare': """
+""",
+    'init': """
+"""
+}
+
+attribute_decl = {
+    'local':
+"""
+    // Local %(attr_type)s %(name)s
+    std::vector< std::vector<%(type)s > > %(name)s;
+""",
+    'global':
+"""
+    // Global %(attr_type)s %(name)s
+    std::vector< %(type)s >  %(name)s ;
+"""
+}
+
+attribute_acc = {
+    'local':
+"""
+    // Local %(attr_type)s %(name)s
+    std::vector<std::vector< %(type)s > > get_%(name)s() { return %(name)s; }
+    std::vector<%(type)s> get_dendrite_%(name)s(int rk) { return %(name)s[rk]; }
+    %(type)s get_synapse_%(name)s(int rk_post, int rk_pre) { return %(name)s[rk_post][rk_pre]; }
+    void set_%(name)s(std::vector<std::vector< %(type)s > >value) { %(name)s = value; }
+    void set_dendrite_%(name)s(int rk, std::vector<%(type)s> value) { %(name)s[rk] = value; }
+    void set_synapse_%(name)s(int rk_post, int rk_pre, %(type)s value) { %(name)s[rk_post][rk_pre] = value; }
+""",
+    'global':
+"""
+    // Global %(attr_type)s %(name)s
+    std::vector<%(type)s> get_%(name)s() { return %(name)s; }
+    %(type)s get_dendrite_%(name)s(int rk) { return %(name)s[rk]; }
+    void set_%(name)s(std::vector<%(type)s> value) { %(name)s = value; }
+    void set_dendrite_%(name)s(int rk, %(type)s value) { %(name)s[rk] = value; }
+"""
+}
+
+attribute_cpp_init = {
+    'local':
+"""
+        // Local %(attr_type)s %(name)s
+        %(name)s = std::vector< std::vector<%(type)s> >(post_rank.size(), std::vector<%(type)s>());
+""",
+    'global':
+"""
+        // Global %(attr_type)s %(name)s
+        %(name)s = std::vector<%(type)s>(post_rank.size(), %(init)s);
+"""
+}
+
+delay = {
+    'declare': """
+    // Non-uniform delay
+    std::vector< std::vector< int > > delay ;""",
+    'pyx_struct':
+"""
+        # Non-uniform delay
+        vector[vector[int]] delay""",
+    'pyx_wrapper_init':
+"""
+        proj%(id_proj)s.delay = syn.delay""",
+    'pyx_wrapper_accessor':
+"""
+    # Access to non-uniform delay
+    def get_delay(self):
+        return proj%(id_proj)s.delay
+    def get_dendrite_delay(self, idx):
+        return proj%(id_proj)s.delay[idx]
+    def set_delay(self, value):
+        proj%(id_proj)s.delay = value
+"""
+}
+
+event_driven = {
+    'declare': """
+    std::vector<std::vector<long> > _last_event;
+""",
+    'cpp_init': """
+""",
+    'pyx_struct': """
+        vector[vector[long]] _last_event
+""",
+    'pyx_wrapper_init':
+"""
+        proj%(id_proj)s._last_event = vector[vector[long]](nb_post, vector[long]())
+        for n in range(nb_post):
+            proj%(id_proj)s._last_event[n] = vector[long](proj%(id_proj)s.nb_synapses(n), -10000)
+"""
+}
+
+conn_templates = {
+    # connectivity
+    'connectivity_matrix': connectivity_matrix,
+    'inverse_connectivity_matrix': inverse_connectivity_matrix,
+    'weight_matrix': weight_matrix,
+    'single_weight_matrix': single_weight_matrix,
+    
+    # accessors
+    'attribute_decl': attribute_decl,
+    'attribute_acc': attribute_acc,
+    'attribute_cpp_init': attribute_cpp_init,
+    'delay': delay,
+    'event_driven': event_driven
+}
