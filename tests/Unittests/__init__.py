d87a0cfab1dfa41c687473c72c5be8f185ab2c02
Helge Dinkelbach <helge.dinkelbach@gmail.com>
2017-03-15 09:41:14 +0100

    + bugfix: if an ITE is part of an ODE, an error in translate_ite occured. + started unittests for ITEs

--- tests/Unittests/__init__.py
+++ tests/Unittests/__init__.py
@@ -5,6 +5,7 @@ from .test_Connectivity import test_Connectivity
 from .test_CustomFunc import test_CustomFunc
 from .test_Dendrite import test_Dendrite
 from .test_GlobalOperation import test_GlobalOps_1D, test_GlobalOps_2D
+from .test_ITE import test_ITE
 from .test_NeuronUpdate import test_LocalUpdate, test_GlobalUpdate
 from .test_Population import test_Population1D, test_Population2D, test_Population3D, test_Population2x3D
 from .test_PopulationView import test_PopulationView
