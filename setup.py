fcc7f037945359de58d51920c412cc564d2250d6
Julien Vitay <julien.vitay@informatik.tu-chemnitz.de>
2019-06-26 12:57:58 +0200

    Intermediary release 4.6.8.1

--- setup.py
+++ setup.py
@@ -288,7 +288,7 @@ dependencies = [
     'sympy'
 ]
 
-release = '4.6.8'
+release = '4.6.8.1'
 print("Installing ANNarchy", release)
 py_version, py_major, python_include, python_libpath, cython_major = python_environment()
 print("\tPython", py_version, "(", sys.executable, ')')
