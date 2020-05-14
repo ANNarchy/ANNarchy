8b1d395f022c31e143edbcadce0255a870b4f408
Julien Vitay <julien.vitay@informatik.tu-chemnitz.de>
2020-05-14 17:10:22 +0200

    Updated doc on notebooks. Deleted Conv1D in convolution.

--- ANNarchy/extensions/convolution/__init__.py
+++ ANNarchy/extensions/convolution/__init__.py
@@ -1,13 +1,14 @@
 """
-In general we assume in ANNarchy a distinct set of weights for each post-synaptic neuron.
-However, operations like pooling or convolution are applied systematically for all pre-neurons within
+ANNarchy uses a distinct set of weights for each post-synaptic neuron (fully-connected).
+However, operations like pooling or convolution are applied with the same weights for all pre-neurons within
 a projection. Consequently, the required weight matrix need to be stored only once. This can
 save computation time and especially memory space.
 
 Contained classes:
 
 * Convolution: simple-, layer-wise and bank of filter convolution
-* Pooling: implements the pooling operation
+* Pooling: pooling operation (max, mean, etc)
+* CopyProjection: mirrors the weights of another projection.
 
 Implementation note:
 
