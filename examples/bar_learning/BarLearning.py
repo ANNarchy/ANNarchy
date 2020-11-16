a2dc286376c8be2406cda11dc5238c85c6a90d13
Helge Dinkelbach <helge.dinkelbach@gmail.com>
2020-11-16 12:51:45 +0100

    removed trailing spaces.

--- examples/bar_learning/BarLearning.py
+++ examples/bar_learning/BarLearning.py
@@ -8,7 +8,7 @@ InputNeuron = Neuron(parameters="r = 0.0")
 
 # Leaky neuron
 LeakyNeuron = Neuron(
-    parameters=""" 
+    parameters="""
         tau = 10.0 : population
     """,
     equations="""
@@ -18,7 +18,7 @@ LeakyNeuron = Neuron(
 
 # Oja synapse
 Oja = Synapse(
-    parameters=""" 
+    parameters="""
         tau = 2000.0 : projection
         alpha = 8.0 : projection
         min_w = 0.0 : projection
@@ -26,7 +26,7 @@ Oja = Synapse(
     equations="""
         tau * dw/dt = pre.r * post.r - alpha * post.r^2 * w : min=min_w
     """
-)  
+)
 
 # Creating the populations
 Input = Population(geometry=(8, 8), neuron=InputNeuron)
@@ -34,18 +34,18 @@ Feature = Population(geometry=(8, 4), neuron=LeakyNeuron)
 
 # Creating the projections
 ff = Projection(
-    pre=Input, 
-    post=Feature, 
-    target='exc', 
-    synapse = Oja    
+    pre=Input,
+    post=Feature,
+    target='exc',
+    synapse = Oja
 )
 ff.connect_all_to_all(weights = Uniform(-0.5, 0.5))
 ff.min_w = -10.0
-                     
+
 lat = Projection(
-    pre=Feature, 
-    post=Feature, 
-    target='inh', 
+    pre=Feature,
+    post=Feature,
+    target='inh',
     synapse = Oja
 )
 lat.connect_all_to_all(weights = Uniform(0.0, 1.0))
@@ -68,8 +68,8 @@ def trial():
     simulate(50.)
     # Return firing rates and receptive fields
     return Input.r, Feature.r, ff.receptive_fields()
-    
-       
+
+
 if __name__=='__main__':
 
     compile()
