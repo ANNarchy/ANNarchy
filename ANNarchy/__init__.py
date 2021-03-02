7a4eee8ce6ee6305175e721a85ec750fff113e5a
Julien Vitay <julien.vitay@informatik.tu-chemnitz.de>
2021-03-02 09:05:02 +0100

    FOrgot setup.py version.

--- ANNarchy/__init__.py
+++ ANNarchy/__init__.py
@@ -39,7 +39,7 @@ from .generator import compile
 
 # Version
 __version__ = '4.6'
-__release__ = '4.6.9.8'
+__release__ = '4.6.9.9'
 
 core.Global._print( 'ANNarchy ' + __version__ + ' (' + __release__ + \
                     ') on ' + sys.platform + ' (' + os.name + ').' )
