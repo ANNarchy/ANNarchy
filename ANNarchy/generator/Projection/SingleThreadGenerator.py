2cf52633a22675c88f3d598732368d29d199870f
Helge Dinkelbach <helge.dinkelbach@gmail.com>
2022-01-18 11:29:26 +0100

    code generation bugfix (commit 92a5ed8): wrong include for specific projections.

--- ANNarchy/generator/Projection/SingleThreadGenerator.py
+++ ANNarchy/generator/Projection/SingleThreadGenerator.py
@@ -138,6 +138,7 @@ class SingleThreadGenerator(ProjectionGenerator):
             # The user is responsible to define the connectivity related variables
             sparse_matrix_format = "SpecificConnectivity"
             sparse_matrix_args = ""
+            sparse_matrix_include = "#include \"Specific.hpp\"\n"
             connector_call = ""
             declare_connectivity_matrix = proj._specific_template['declare_connectivity_matrix']
             access_connectivity_matrix = proj._specific_template['access_connectivity_matrix']
