a94ab9491a8c65793cf91ca9c9b1f6592c372c8c
Julien Vitay <julien.vitay@informatik.tu-chemnitz.de>
2022-04-06 14:22:08 +0200

    Release 4.7.1.1

--- ANNarchy/__init__.py
+++ ANNarchy/__init__.py
@@ -46,7 +46,7 @@ atexit.register(clear)
 
 # Version
 __version__ = '4.7'
-__release__ = '4.7.1'
+__release__ = '4.7.1.1'
 
 print( 'ANNarchy ' + __version__ + ' (' + __release__ + \
                     ') on ' + sys.platform + ' (' + os.name + ').' )
