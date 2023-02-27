e1a5f2094989bb43a751d9b00036ff69fd75a52b
Helge Dinkelbach <helge.dinkelbach@gmail.com>
2023-02-27 07:40:30 +0100

    Same as in commit 6ca3a72 for pre_to_post case.

--- ANNarchy/generator/Projection/CUDAGenerator.py
+++ ANNarchy/generator/Projection/CUDAGenerator.py
@@ -657,10 +657,12 @@ class CUDAGenerator(ProjectionGenerator):
                 raise NotImplementedError
 
         elif proj._storage_format == "dense":
+            # HD (27th Feb. 2023): the dense matrix has no explicit indices, as the position is
+            #                      computed inside the kernel.
             if proj._storage_order == "post_to_pre":
                 pass
             else:
-                raise NotImplementedError
+                pass
 
         else:
             # just a reminder to check indices for new formats
