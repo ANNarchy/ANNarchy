fadc21f1c62e9c55631aaca3c58ac0d65e28d37b
Helge Dinkelbach <helge.dinkelbach@gmail.com>
2023-06-02 14:16:54 +0200

    Removed deprecated "weightsharing" extension (replaced by "convolution" since 4.6.8 release).

--- ANNarchy/extensions/weightsharing/__init__.py
+++
@@ -1,4 +0,0 @@
-from ANNarchy.core import Global
-from .SharedProjection import SharedProjection
-
-Global._warning("ANNarchy.extensions.weightsharing is deprecated since 4.6.8, use ANNarchy.extensions.convolution instead.")
