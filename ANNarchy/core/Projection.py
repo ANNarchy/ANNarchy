5d31e63da1219e65cc10b12922c9b61a4f60bcb0
Helge Dinkelbach <helge.dinkelbach@gmail.com>
2023-08-10 06:33:51 +0200

    Updated heuristics: use dense_t/csrc_t for spiking models on CUDA devices.

--- ANNarchy/core/Projection.py
+++ ANNarchy/core/Projection.py
@@ -504,10 +504,7 @@ class Projection(object):
                 # get the decision parameter
                 density = float(self._lil_connectivity.nb_synapses) / float(self.pre.size * self.post.size)
                 if density >= 0.6:
-                    if Global._check_paradigm("cuda"):
-                        storage_format = "csr"  # HD (11th Nov. 2022): there is no Dense_T for spiking and CUDA yet
-                    else:
-                        storage_format = "dense"
+                    storage_format = "dense"
                 else:
                     storage_format = "csr"
 
@@ -542,15 +539,11 @@ class Projection(object):
         if self.synapse_type.type == "rate":
             storage_order = "post_to_pre"
         else:
-            if Global._check_paradigm("cuda"):
-                # HD (11th Nov. 2022): there is no Dense_T / CSRC_T for spiking and CUDA yet
-                storage_order = "post_to_pre"
+            # pre-to-post is not implemented for all formats
+            if self._storage_format in ["dense", "csr"]:
+                storage_order = "pre_to_post"
             else:
-                # pre-to-post is not implemented for all formats
-                if self._storage_format in ["dense", "csr"]:
-                    storage_order = "pre_to_post"
-                else:
-                    storage_order = "post_to_pre"
+                storage_order = "post_to_pre"
 
         Global._info("Automatic matrix order selection for", self.name, ":", storage_order)
         return storage_order
