ANNarchy/ANNarchy

commit 09d5cc0da0292a939dfe1285d997e5e158e708da

Continued implementation of ConfigManager: num_threads, visible_cores, paradigm

ANNarchy/generator/CodeGenerator.py
@@ -54,31 +54,31 @@ class CodeGenerator(object):
         # Profiling is optional, but if either Global.config["profiling"] set to True
         # or --profile was added on command line.
         if get_global_config('profiling'):
-            if Global.config['paradigm'] == "openmp":
+            if get_global_config('paradigm') == "openmp":
                 self._profgen = Profile.CPP11Profile(self._annarchy_dir, net_id)
                 self._profgen.generate()
-            elif Global.config['paradigm'] == "cuda":
+            elif get_global_config('paradigm') == "cuda":
                 self._profgen = Profile.CUDAProfile(self._annarchy_dir, net_id)
                 self._profgen.generate()
             else:
                 Messages._error('No ProfileGenerator available for '
-                              + Global.config['paradigm'])
+                              + get_global_config('paradigm'))
         else:
             self._profgen = None
 
         # Instantiate code generator based on the target platform
-        if Global.config['paradigm'] == "openmp":
-            if Global.config['num_threads'] == 1:
+        if get_global_config('paradigm') == "openmp":
+            if get_global_config('num_threads') == 1:
                 self._popgen = SingleThreadGenerator(self._profgen, net_id)
                 self._projgen = SingleThreadProjectionGenerator(self._profgen, net_id)
             else:
                 self._popgen = OpenMPGenerator(self._profgen, net_id)
                 self._projgen = OpenMPProjectionGenerator(self._profgen, net_id)
-        elif Global.config['paradigm'] == "cuda":
+        elif get_global_config('paradigm') == "cuda":
             self._popgen = CUDAGenerator(self._profgen, net_id)
             self._projgen = CUDAProjectionGenerator(self._profgen, net_id)
         else:
-            Messages._error("No PopulationGenerator for " + Global.config['paradigm'])
+            Messages._error("No PopulationGenerator for " + get_global_config('paradigm'))
 
         # Py-extenstion and RecordGenerator are commonly defined
         self._pyxgen = PyxGenerator(annarchy_dir, populations, projections, net_id)
@@ -110,12 +110,12 @@ class CodeGenerator(object):
             t0 = time.time()
 
         if get_global_config('verbose'):
-            if Global.config['paradigm'] == "openmp":
-                if Global.config['num_threads'] > 1:
+            if get_global_config('paradigm') == "openmp":
+                if get_global_config('num_threads') > 1:
                     Messages._print('\nGenerate code for OpenMP ...')
                 else:
                     Messages._print('\nGenerate sequential code ...')
-            elif Global.config['paradigm'] == "cuda":
+            elif get_global_config('paradigm') == "cuda":
                 print('\nGenerate CUDA code ...')
             else:
                 raise NotImplementedError
@@ -143,11 +143,11 @@ class CodeGenerator(object):
         source_dest = self._annarchy_dir+'/generate/net'+str(self._net_id)+'/'
 
         # Generate header code for the analysed pops and projs
-        if Global.config['paradigm'] == "openmp":
+        if get_global_config('paradigm') == "openmp":
             with open(source_dest+'ANNarchy.h', 'w') as ofile:
                 ofile.write(self._generate_header())
 
-        elif Global.config['paradigm'] == "cuda":
+        elif get_global_config('paradigm') == "cuda":
             invoke_header, host_header = self._generate_header()
             with open(source_dest+'ANNarchyKernel.cuh', 'w') as ofile:
                 ofile.write(invoke_header)
@@ -161,11 +161,11 @@ class CodeGenerator(object):
         self._recordgen.generate()
 
         # Generate cpp code for the analysed pops and projs
-        if Global.config['paradigm'] == "openmp":
+        if get_global_config('paradigm') == "openmp":
             with open(source_dest+'ANNarchy.cpp', 'w') as ofile:
                 ofile.write(self._generate_body())
 
-        elif Global.config['paradigm'] == "cuda":
+        elif get_global_config('paradigm') == "cuda":
             device_code, host_code = self._generate_body()
             with open(source_dest+'ANNarchy.cpp', 'w') as ofile:
                 ofile.write(host_code)
@@ -295,7 +295,7 @@ class CodeGenerator(object):
 
         # Final code
         header_code = ""
-        if Global.config['paradigm'] == "openmp":
+        if get_global_config('paradigm') == "openmp":
             header_code = BaseTemplate.omp_header_template % {
                 'float_prec': get_global_config('precision'),
                 'pop_struct': pop_struct,
@@ -308,7 +308,7 @@ class CodeGenerator(object):
             }
             return header_code
 
-        elif Global.config['paradigm'] == "cuda":
+        elif get_global_config('paradigm') == "cuda":
             # kernel declaration
             invoke_kernel_def = ""
             for pop in self._pop_desc:
@@ -533,7 +533,7 @@ void set_%(name)s(%(float_prec)s value) {
         # greatly. For further information take a look into the corresponding
         # branches.
         #
-        if Global.config['paradigm'] == "openmp":
+        if get_global_config('paradigm') == "openmp":
             # custom constants
             custom_constant, _ = self._body_custom_constants()
 
@@ -561,12 +561,12 @@ void set_%(name)s(%(float_prec)s value) {
             base_dict.update(prof_dict)
 
             # complete code template
-            if Global.config["num_threads"] == 1:
+            if get_global_config('num_threads') == 1:
                 return BaseTemplate.st_body_template % base_dict
             else:
                 return BaseTemplate.omp_body_template % base_dict
 
-        elif Global.config['paradigm'] == "cuda":
+        elif get_global_config('paradigm') == "cuda":
             # Implementation notice ( HD: 10. June, 2015 )
             #
             # The CUDA linking process is a big problem for object oriented approaches
@@ -735,12 +735,12 @@ void set_%(name)s(%(float_prec)s value) {
             projection_init += proj['init']
 
         # Initialize custom constants
-        if Global.config['paradigm'] == "openmp":
+        if get_global_config('paradigm') == "openmp":
             # Custom  constants
             _, custom_constant = self._body_custom_constants()
 
             init_tpl = BaseTemplate.omp_initialize_template
-        elif Global.config['paradigm'] == "cuda":
+        elif get_global_config('paradigm') == "cuda":
             # Custom  constants
             _, custom_constant = self._body_custom_constants()
 
@@ -813,7 +813,7 @@ void set_%(name)s(%(float_prec)s value) {
 
         # the computation kernel depends on the paradigm
         if Global._check_paradigm("openmp"):
-            if Global.config["num_threads"] == 1:
+            if get_global_config('num_threads') == 1:
                 global_op_template = global_operation_templates_st
             else:
                 global_op_template = global_operation_templates_openmp

ANNarchy/generator/Profile/CPP11Profile.py
@@ -30,7 +30,7 @@ class CPP11Profile(ProfileGenerator):
         """
         Creates a dictionary, contain profile code snippets.
         """
-        if Global.config["num_threads"] == 1:
+        if get_global_config('num_threads') == 1:
             body_dict = {
                 'prof_include': cpp11_profile_template['include'],
                 'prof_step_pre': cpp11_profile_template['step_pre'],
@@ -78,7 +78,7 @@ class CPP11Profile(ProfileGenerator):
         return body_dict
 
     def generate_init_network(self):
-        if Global.config["num_threads"] == 1:
+        if get_global_config('num_threads') == 1:
             return cpp11_profile_template['init']
         else:
             return cpp11_omp_profile_template['init']
@@ -131,7 +131,7 @@ class CPP11Profile(ProfileGenerator):
         """
         annotate the computesum compuation code
         """
-        if Global.config["num_threads"] == 1:
+        if get_global_config('num_threads') == 1:
             prof_begin = cpp11_profile_template['compute_psp']['before']
             prof_end = cpp11_profile_template['compute_psp']['after']
         else:
@@ -155,7 +155,7 @@ class CPP11Profile(ProfileGenerator):
         """
         annotate the computesum compuation code
         """
-        if Global.config["num_threads"] == 1:
+        if get_global_config('num_threads') == 1:
             prof_begin = cpp11_profile_template['compute_psp']['before'] % {'name': 'proj'+str(proj.id)}
             prof_end = cpp11_profile_template['compute_psp']['after'] % {'name': 'proj'+str(proj.id)}
         else:
@@ -177,7 +177,7 @@ class CPP11Profile(ProfileGenerator):
         """
         annotate the update synapse code, generated by ProjectionGenerator.update_synapse()
         """
-        if Global.config["num_threads"] == 1:        
+        if get_global_config('num_threads') == 1:        
             prof_begin = cpp11_profile_template['update_synapse']['before']
             prof_end = cpp11_profile_template['update_synapse']['after']
         else:
@@ -200,7 +200,7 @@ class CPP11Profile(ProfileGenerator):
         """
         annotate the post-event code
         """
-        if Global.config["num_threads"] == 1:
+        if get_global_config('num_threads') == 1:
             prof_begin = cpp11_profile_template['post_event']['before']
             prof_end = cpp11_profile_template['post_event']['after']
         else:
@@ -224,7 +224,7 @@ class CPP11Profile(ProfileGenerator):
         """
         annotate the update neuron code
         """
-        if Global.config["num_threads"] == 1:        
+        if get_global_config('num_threads') == 1:        
             prof_begin = cpp11_profile_template['update_neuron']['before'] % {'name': pop.name}
             prof_end = cpp11_profile_template['update_neuron']['after'] % {'name': pop.name}
         else:
@@ -246,7 +246,7 @@ class CPP11Profile(ProfileGenerator):
         """
         annotate the spike condition code
         """
-        if Global.config["num_threads"] == 1:
+        if get_global_config('num_threads') == 1:
             prof_begin = cpp11_profile_template['spike_gather']['before'] % {'name': pop.name}
             prof_end = cpp11_profile_template['spike_gather']['after'] % {'name': pop.name}
         else:
@@ -270,7 +270,7 @@ class CPP11Profile(ProfileGenerator):
         """
         annotate update rng kernel (only for CPUs available)
         """
-        if Global.config["num_threads"] == 1:
+        if get_global_config('num_threads') == 1:
             prof_begin = cpp11_profile_template['update_rng']['before'] % {'name': pop.name}
             prof_end = cpp11_profile_template['update_rng']['after'] % {'name': pop.name}
         else:
@@ -293,7 +293,7 @@ class CPP11Profile(ProfileGenerator):
         """
         annotate update delay kernel (only for CPUs available)
         """
-        if Global.config["num_threads"] == 1:
+        if get_global_config('num_threads') == 1:
             prof_begin = cpp11_profile_template['update_delay']['before'] % {'name': pop.name}
             prof_end = cpp11_profile_template['update_delay']['after'] % {'name': pop.name}
         else:
@@ -323,9 +323,9 @@ class CPP11Profile(ProfileGenerator):
         _out_file << "  </config>" << std::endl;
         """ % {
             'paradigm': Global.config["paradigm"],
-            'num_threads': Global.config["num_threads"]
+            'num_threads': get_global_config('num_threads')
         }
-        config = Global.config["paradigm"] + '_'  + str(Global.config["num_threads"]) + 'threads'
+        config = Global.config["paradigm"] + '_'  + str(get_global_config('num_threads')) + 'threads'
 
         timer_import = "#include <chrono>"
         timer_start = "std::chrono::time_point<std::chrono::steady_clock> _profiler_start;"

ANNarchy/generator/Profile/PAPIProfile.py
@@ -158,7 +158,7 @@ class PAPIProfile(ProfileGenerator):
         _out_file << "  </config>" << std::endl;
         """ % {
             'paradigm': Global.config["paradigm"],
-            'num_threads': Global.config["num_threads"]
+            'num_threads': get_global_config('num_threads')
         }
 
         timer_import = "#include <papi.h>"
@@ -171,7 +171,7 @@ class PAPIProfile(ProfileGenerator):
         _profiler_start = PAPI_get_real_usec();
 """
 
-        config = Global.config["paradigm"] + '_'  + str(Global.config["num_threads"]) + 'threads'
+        config = Global.config["paradigm"] + '_'  + str(get_global_config('num_threads')) + 'threads'
         return profile_base_template % {
             'timer_import': timer_import,
             'timer_start_decl': timer_start,

ANNarchy/core/Global.py
@@ -26,9 +26,6 @@ _objects = {
 config = dict(
    {
     'debug': False,       
-    'num_threads': 1,
-    'visible_cores': [],
-    'paradigm': "openmp",
     'disable_shared_library_time_offset': False
    }
 )
@@ -465,7 +462,7 @@ def set_seed(seed, use_seed_seq=True, net_id=0):
         if get_global_config('disable_parallel_rng'):
             NetworkManager().cy_instance(net_id=net_id).set_seed(seed, 1, use_seed_seq)
         else:
-            NetworkManager().cy_instance(net_id=net_id).set_seed(seed, config['num_threads'], use_seed_seq)
+            NetworkManager().cy_instance(net_id=net_id).set_seed(seed, get_global_config('num_threads'), use_seed_seq)
     except:
         Messages._warning('The seed will only be set in the simulated network when it is compiled.')
 
@@ -483,7 +480,7 @@ def _check_paradigm(paradigm):
     2. "cuda"
     """
     try:
-        return paradigm == config['paradigm']
+        return paradigm == get_global_config('paradigm')
     except KeyError:
         Messages._error("Unknown paradigm")
 

ANNarchy/core/Network.py
@@ -658,9 +658,9 @@ def _parallel_networks(method, networks, max_processes, measure_time, sequential
     # Number of processes to create depends on number of
     # available CPUs or GPUs
     if max_processes < 0:
-        if Global.config['paradigm'] == "openmp":
+        if get_global_config('paradigm') == "openmp":
             max_processes = min(len(networks), multiprocessing.cpu_count())
-        elif Global.config['paradigm'] == "cuda":
+        elif get_global_config('paradigm') == "cuda":
             Messages._warning("In the present ANNarchy version the usage of parallel networks and multi-GPUs is disabled.")
             max_processes = 1
         else:
@@ -731,9 +731,9 @@ def _parallel_multi(method, number, max_processes, measure_time, sequential, sam
 
     # Number of processes to create
     if max_processes < 0:
-        if Global.config['paradigm'] == "openmp":
+        if get_global_config('paradigm') == "openmp":
             max_processes = min(number, multiprocessing.cpu_count())
-        elif Global.config['paradigm'] == "cuda":
+        elif get_global_config('paradigm') == "cuda":
             Messages._warning("In the present ANNarchy version the usage of parallel networks and multi-GPUs is disabled.")
             max_processes = 1
         else:

ANNarchy/core/Projection.py
@@ -565,7 +565,7 @@ class Projection :
 
     def _has_single_weight(self):
         "If a single weight should be generated instead of a LIL"
-        is_cpu = Global.config['paradigm']=="openmp"
+        is_cpu = get_global_config('paradigm')=="openmp"
         has_constant_weight = self._single_constant_weight
         not_dense = not (self._storage_format == "dense")
         no_structural_plasticity = not get_global_config('structural_plasticity')

ANNarchy/extensions/convolution/Convolve.py
@@ -547,7 +547,7 @@ class Convolution(SpecificProjection):
 
         # OMP code
         omp_code = ""
-        if Global.config['num_threads'] > 1:
+        if get_global_config('num_threads') > 1:
             omp_code = """
         #pragma omp for private(sum, rk_pre, coord) %(psp_schedule)s""" % {'psp_schedule': "" if not 'psp_schedule' in self._omp_config.keys() else self._omp_config['psp_schedule']}
 

ANNarchy/extensions/convolution/Copy.py
@@ -147,7 +147,7 @@ class Copy(SpecificProjection):
         self._specific_template.update(copy_proj_dict)
 
         # OMP code if more then one thread
-        if Global.config['num_threads'] > 1:
+        if get_global_config('num_threads') > 1:
             omp_code = '#pragma omp for private(sum)' if self.post.size > Global.OMP_MIN_NB_NEURONS else ''
         else:
             omp_code = ""

commit 4d2e1caebe61bd2492236114f6ba6b08c2b43cfd

Fixed bug introduced by commit 09d5cc0.

ANNarchy/generator/CodeGenerator.py
@@ -805,7 +805,7 @@ void set_%(name)s(%(float_prec)s value) {
             elif _check_paradigm("cuda"):
                 return "", "", ""
             else:
-                raise NotImplementedError("CodeGenerator._body_def_glops(): no implementation for "+Global.config["paradigm"])
+                raise NotImplementedError("CodeGenerator._body_def_glops(): no implementation for "+get_global_config('paradigm'))
 
         type_def = {
             'type': get_global_config('precision')
@@ -836,7 +836,7 @@ void set_%(name)s(%(float_prec)s value) {
 
             return header, invoke, body
         else:
-            raise NotImplementedError("CodeGenerator._body_def_glops(): no implementation for "+Global.config["paradigm"])
+            raise NotImplementedError("CodeGenerator._body_def_glops(): no implementation for "+get_global_config('paradigm'))
 
     def _body_run_until(self):
         """

ANNarchy/generator/Profile/CPP11Profile.py
@@ -3,7 +3,6 @@
 :license: GPLv2, see LICENSE for details.
 """
 
-from ANNarchy.core import Global
 from ANNarchy.generator.Utils import tabify
 from ANNarchy.intern.ConfigManagement import get_global_config
 
@@ -322,10 +321,10 @@ class CPP11Profile(ProfileGenerator):
         _out_file << "    <num_threads>%(num_threads)s</num_threads>" << std::endl;
         _out_file << "  </config>" << std::endl;
         """ % {
-            'paradigm': Global.config["paradigm"],
+            'paradigm': get_global_config('paradigm'),
             'num_threads': get_global_config('num_threads')
         }
-        config = Global.config["paradigm"] + '_'  + str(get_global_config('num_threads')) + 'threads'
+        config = get_global_config('paradigm') + '_'  + str(get_global_config('num_threads')) + 'threads'
 
         timer_import = "#include <chrono>"
         timer_start = "std::chrono::time_point<std::chrono::steady_clock> _profiler_start;"

ANNarchy/generator/Profile/CUDAProfile.py
@@ -3,7 +3,7 @@
 :license: GPLv2, see LICENSE for details.
 """
 
-from ANNarchy.core import Global
+from ANNarchy.intern.ConfigManagement import get_global_config
 
 from .ProfileGenerator import ProfileGenerator
 from .ProfileTemplate import profile_base_template, cuda_profile_template, cuda_profile_header
@@ -199,7 +199,7 @@ class CUDAProfile(ProfileGenerator):
         _out_file << "  <config>" << std::endl;
         _out_file << "    <paradigm>%(paradigm)s</paradigm>" << std::endl;
         _out_file << "  </config>" << std::endl;
-        """ % {'paradigm': Global.config["paradigm"]}
+        """ % {'paradigm': get_global_config('paradigm')}
 
         timer_import = "#include <cuda_runtime_api.h>"
         timer_start = "cudaEvent_t _profiler_start;"
@@ -208,7 +208,7 @@ class CUDAProfile(ProfileGenerator):
         cudaEventRecord(_profiler_start);
 """
 
-        config = Global.config["paradigm"]
+        config = get_global_config('paradigm')
         return profile_base_template % {
             'timer_import': timer_import,
             'timer_start_decl': timer_start,

ANNarchy/generator/Profile/PAPIProfile.py
@@ -3,7 +3,6 @@
 :license: GPLv2, see LICENSE for details.
 """
 
-from ANNarchy.core import Global
 from ANNarchy.intern.ConfigManagement import get_global_config
 from ANNarchy.intern import Messages
 
@@ -157,7 +156,7 @@ class PAPIProfile(ProfileGenerator):
         _out_file << "    <num_threads>%(num_threads)s</num_threads>" << std::endl;
         _out_file << "  </config>" << std::endl;
         """ % {
-            'paradigm': Global.config["paradigm"],
+            'paradigm': get_global_config('paradigm'),
             'num_threads': get_global_config('num_threads')
         }
 
@@ -171,7 +170,7 @@ class PAPIProfile(ProfileGenerator):
         _profiler_start = PAPI_get_real_usec();
 """
 
-        config = Global.config["paradigm"] + '_'  + str(get_global_config('num_threads')) + 'threads'
+        config = get_global_config('paradigm') + '_'  + str(get_global_config('num_threads')) + 'threads'
         return profile_base_template % {
             'timer_import': timer_import,
             'timer_start_decl': timer_start,