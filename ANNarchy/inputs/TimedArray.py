ANNarchy/ANNarchy

commit ef374036f7a998afcd5c0304a2a523b72611e4ec

TimedArray: fixed a segmentation fault, when the rates has not been set. Also, added unittests for late initialization of the TimedArray buffer.

ANNarchy/inputs/TimedArray.py
@@ -238,7 +238,21 @@ class TimedArray(SpecificPopulation):
         #endif
 
             // Check if it is time to set the input
-            if(_t == _schedule[_block]){
+            if (_t == _schedule[_block]) {
+                // sanity check
+                if (_buffer.empty()) {
+                    std::cerr << "TimedArray: no data being set ..." << std::endl;
+                    r = std::vector<%(float_prec)s>(size, 0.0);
+                    return;
+                }
+
+                // sanity check
+                if (_buffer.size() <= _block) {
+                    std::cerr << "TimedArray: not enough data being set ..." << std::endl;
+                    r = std::vector<%(float_prec)s>(size, 0.0);
+                    return;
+                }
+
                 // Set the data
                 r = _buffer[_block];
                 // Move to the next block
@@ -263,7 +277,7 @@ class TimedArray(SpecificPopulation):
             std::cout << "TimedArray::update(t="<< t <<") - current buffer (min/max) = [" << *std::min_element(r.begin(), r.end()) << "," << *std::max_element(r.begin(), r.end()) <<  "]" << std::endl;
         #endif
         }
-"""
+""" % {'float_prec': get_global_config('precision')}
 
         self._specific_template['size_in_bytes'] = """
         // schedule
@@ -338,6 +352,9 @@ class TimedArray(SpecificPopulation):
         return pop%(id)s.get_period()
 """ % { 'id': self.id }
 
+        # HD (28th Jun. 24): contrary to the single-thread codes, where we use 'return' to escape the function
+        #                    execution, OpenMP does not allow 'return'/'continue' in the execution block. Therefore,
+        #                    we need to use the if-else tree.
         self._specific_template['update_variables'] = """
         if(_active){
             #pragma omp single
@@ -347,14 +364,29 @@ class TimedArray(SpecificPopulation):
             #endif
 
                 // Check if it is time to set the input
-                if(_t == _schedule[_block]){
-                    // Set the data
-                    r = _buffer[_block];
-                    // Move to the next block
-                    _block++;
-                    // If was the last block, go back to the first block
-                    if (_block == _schedule.size()){
-                        _block = 0;
+                if (_t == _schedule[_block]) {
+                    // sanity check
+                    if (_buffer.empty()) {
+                        std::cerr << "TimedArray: no data being set ..." << std::endl;
+                        r = std::vector<%(float_prec)s>(size, 0.0);
+                    }
+
+                    // sanity check
+                    else if (_buffer.size() <= _block) {
+                        std::cerr << "TimedArray: not enough data being set ..." << std::endl;
+                        r = std::vector<%(float_prec)s>(size, 0.0);
+                    }
+
+                    // everything appears right, proceed
+                    else {
+                        // Set the data
+                        r = _buffer[_block];
+                        // Move to the next block
+                        _block++;
+                        // If was the last block, go back to the first block
+                        if (_block == _schedule.size()){
+                            _block = 0;
+                        }
                     }
                 }
 
@@ -369,7 +401,7 @@ class TimedArray(SpecificPopulation):
                 _t++;
             }
         }
-"""
+""" % {'float_prec': get_global_config('precision')}
 
         self._specific_template['size_in_bytes'] = """
         // schedule
@@ -489,7 +521,21 @@ class TimedArray(SpecificPopulation):
             std::cout << "TimedArray::update() - " << _t << " " << _block<< " " << _schedule[_block] << std::endl;
         #endif
             // Check if it is time to set the input
-            if(_t == _schedule[_block]){
+            if (_t == _schedule[_block]) {
+                // sanity check
+                if (_buffer.empty()) {
+                    std::cerr << "TimedArray: no data being set ..." << std::endl;
+                    gpu_r = gpu_buffer[0];
+                    return;
+                }
+
+                // sanity check
+                if (_buffer.size() <= _block) {
+                    std::cerr << "TimedArray: not enough data being set ..." << std::endl;
+                    gpu_r = gpu_buffer[0];
+                    return;
+                }
+
                 // Set the data
                 gpu_r = gpu_buffer[_block];
                 // Move to the next block

tests/Interface/__init__.py
@@ -1,4 +1,4 @@
 import unittest
 from .test_IO import test_IO_Rate, test_IO_Spiking
 from .test_Report import test_Report_Rate, test_Report_Spiking
-from .test_TimedArray import test_TimedArray, test_TimedArrayUpdate
+from .test_TimedArray import test_TimedArray, test_TimedArrayLateInit, test_TimedArrayUpdate

tests/Interface/test_TimedArray.py
@@ -142,6 +142,129 @@ class test_TimedArray(unittest.TestCase):
 
         np.testing.assert_allclose(self.output.r4, [0, 1, 0, 0, 0, 0, 0, 0, 0, 0])
 
+class test_TimedArrayLateInit(unittest.TestCase):
+    """
+    Test the correct implementation of late-initialized timed arrays.
+    """
+    @classmethod
+    def setUpClass(cls):
+        """
+        Compile the network for this test. Adapted the example
+        from documentation ( section 3.7.3 Class TimedArray )
+        """
+        inputs = np.array(
+            [
+                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+            ]
+        )
+
+        SimpleNeuron = Neuron(
+            equations="""
+                r = sum(exc)
+                r2 = sum(exc2)
+                r3 = sum(exc3)
+                r4 = sum(exc4)
+            """
+        )
+
+        inp = TimedArray(geometry=(10))
+        inp2 = TimedArray(geometry=(10))
+        inp3 = TimedArray(geometry=(10))
+        inp4 = TimedArray(geometry=(10))
+
+        inp.update(rates=inputs)
+        inp2.update(rates=inputs, schedule=10.)
+        inp3.update(rates=inputs, period=10.)
+        inp4.update(rates=inputs, schedule=2., period=20.)
+
+        pop = Population(10, neuron=SimpleNeuron)
+
+        proj = Projection(inp, pop, 'exc')
+        proj.connect_one_to_one(1.0)
+        proj2 = Projection(inp2, pop, 'exc2')
+        proj2.connect_one_to_one(1.0)
+        proj3 = Projection(inp3, pop, 'exc3')
+        proj3.connect_one_to_one(1.0)
+        proj4 = Projection(inp4, pop, 'exc4')
+        proj4.connect_one_to_one(1.0)
+
+        cls.test_net = Network()
+        cls.test_net.add([inp, inp2, inp3, inp4, pop, proj, proj2, proj3, proj4])
+        cls.test_net.compile(silent=True)
+
+        cls.output = cls.test_net.get(pop)
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        All tests of this class are done. We can destroy the network.
+        """
+        del cls.test_net
+        clear()
+
+    def setUp(self):
+        """
+        Automatically called before each test method, basically to reset the network after every test.
+        """
+        self.test_net.reset()
+
+    def test_compile(self):
+        """
+        Enforce compilation of the network.
+        """
+        pass
+
+    def test_run_one_loop(self):
+        """
+        We provide 10 ms input data, after 11ms the last element will be set.
+        """
+        self.test_net.simulate(11)
+        np.testing.assert_allclose(self.output.r, [0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
+
+    def test_more_steps(self):
+        """
+        We provide 10 ms input data, after 11ms the last element will be remain in the buffer.
+        """
+        self.test_net.simulate(12)
+        np.testing.assert_allclose(self.output.r, [0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
+
+    def test_run_one_schedule(self):
+        """
+        We provide 10 ms input data the 1st entry, after 11ms the 2nd element will be set. So the next 10
+        iterations should return the 2nd element of the input array.
+        """
+        self.test_net.simulate(11) # 1st entry
+
+        self.test_net.simulate(10) # 2nd entry
+        np.testing.assert_allclose(self.output.r2, [0, 1, 0, 0, 0, 0, 0, 0, 0, 0])
+
+    def test_run_one_period(self):
+        """
+        Period is set to 10, this means after 11 ms we have one cycle complete. This means the 12 step
+        should return the 1st element in the output population.
+        """
+        self.test_net.simulate(12) # 1st entry
+
+        np.testing.assert_allclose(self.output.r3, [1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+    def test_run_one_period_one_schedule(self):
+        """
+        Schedule == 2 doubles all entries. Period of 20 is one complete iteration. This means
+        after 25 steps we are again on the 2nd entry.
+        """
+        self.test_net.simulate(25)
+
+        np.testing.assert_allclose(self.output.r4, [0, 1, 0, 0, 0, 0, 0, 0, 0, 0])
+
 class test_TimedArrayUpdate(unittest.TestCase):
     """
     This class tests the change of the input array after some simulation steps.

commit 3fd6b2572f6430c454dfb14bc06ba0885e25ac7a

Bugfix: fixed CUDA code error introduced in commit ef37403.

ANNarchy/inputs/TimedArray.py
@@ -430,7 +430,7 @@ class TimedArray(SpecificPopulation):
         self._specific_template['declare_additional'] = """
     // Custom local parameter timed array
     std::vector< int > _schedule;
-    std::vector< %(float_prec)s* > gpu_buffer;
+    std::vector< %(float_prec)s* > _gpu_buffer;
     int _period; // Period of cycling
     long int _t; // Internal time
     int _block; // Internal block when inputs are set not at each step
@@ -440,27 +440,29 @@ class TimedArray(SpecificPopulation):
     void set_schedule(std::vector<int> schedule) { _schedule = schedule; }
     std::vector<int> get_schedule() { return _schedule; }
     void set_buffer(std::vector< std::vector< %(float_prec)s > > buffer) {
-        if ( gpu_buffer.empty() ) {
-            gpu_buffer = std::vector< %(float_prec)s* >(buffer.size(), nullptr);
+        if ( _gpu_buffer.empty() ) {
+            // host holds a set of pointers
+            _gpu_buffer = std::vector< %(float_prec)s* >(buffer.size(), nullptr);
+
             // allocate gpu arrays
             for(int i = 0; i < buffer.size(); i++) {
-                cudaMalloc((void**)&gpu_buffer[i], buffer[i].size()*sizeof(%(float_prec)s));
+                cudaMalloc((void**)&_gpu_buffer[i], buffer[i].size()*sizeof(%(float_prec)s));
             }
         }
 
         auto host_it = buffer.begin();
-        auto dev_it = gpu_buffer.begin();
+        auto dev_it = _gpu_buffer.begin();
         for (; host_it < buffer.end(); host_it++, dev_it++) {
             cudaMemcpy( *dev_it, host_it->data(), host_it->size()*sizeof(%(float_prec)s), cudaMemcpyHostToDevice);
         }
 
-        gpu_r = gpu_buffer[_block];
+        gpu_r = _gpu_buffer[_block];
     }
     std::vector< std::vector< %(float_prec)s > > get_buffer() {
-        std::vector< std::vector< %(float_prec)s > > buffer = std::vector< std::vector< %(float_prec)s > >( gpu_buffer.size(), std::vector<%(float_prec)s>(size,0.0) );
+        std::vector< std::vector< %(float_prec)s > > buffer = std::vector< std::vector< %(float_prec)s > >( _gpu_buffer.size(), std::vector<%(float_prec)s>(size,0.0) );
 
         auto host_it = buffer.begin();
-        auto dev_it = gpu_buffer.begin();
+        auto dev_it = _gpu_buffer.begin();
         for (; host_it < buffer.end(); host_it++, dev_it++) {
             cudaMemcpy( host_it->data(), *dev_it, size*sizeof(%(float_prec)s), cudaMemcpyDeviceToHost );
         }
@@ -480,7 +482,7 @@ class TimedArray(SpecificPopulation):
         // counters
         _t = 0;
         _block = 0;
-        gpu_r = gpu_buffer[0];
+        gpu_r = _gpu_buffer[0];
 """
         self._specific_template['export_additional'] = """
         # Custom local parameters timed array
@@ -523,21 +525,21 @@ class TimedArray(SpecificPopulation):
             // Check if it is time to set the input
             if (_t == _schedule[_block]) {
                 // sanity check
-                if (_buffer.empty()) {
+                if (_gpu_buffer.empty()) {
                     std::cerr << "TimedArray: no data being set ..." << std::endl;
-                    gpu_r = gpu_buffer[0];
+                    gpu_r = _gpu_buffer[0];
                     return;
                 }
 
                 // sanity check
-                if (_buffer.size() <= _block) {
+                if (_gpu_buffer.size() <= _block) {
                     std::cerr << "TimedArray: not enough data being set ..." << std::endl;
-                    gpu_r = gpu_buffer[0];
+                    gpu_r = _gpu_buffer[0];
                     return;
                 }
 
                 // Set the data
-                gpu_r = gpu_buffer[_block];
+                gpu_r = _gpu_buffer[_block];
                 // Move to the next block
                 _block++;
                 // If was the last block, go back to the first block
@@ -574,7 +576,7 @@ class TimedArray(SpecificPopulation):
 
         // gpu_buffer
         size_in_bytes += sizeof(std::vector<%(float_prec)s*>);
-        size_in_bytes += gpu_buffer.capacity() * sizeof(%(float_prec)s*);
+        size_in_bytes += _gpu_buffer.capacity() * sizeof(%(float_prec)s*);
 """ % {'float_prec': get_global_config('precision')}
 
     def _instantiate(self, module):