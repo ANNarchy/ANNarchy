ANNarchy/ANNarchy

commit bdc4dd110c34267259adcd7fb3f9727356c48438

Started to replace Cython by nanobind. In this commit I adapted the Populations and Monitors for Population on CPUs.

ANNarchy/generator/Compiler.py
@@ -15,7 +15,6 @@ import numpy as np
 import ANNarchy
 
 from ANNarchy.intern.NetworkManager import NetworkManager
-from ANNarchy.core import Global
 from ANNarchy.intern.Profiler import Profiler
 from ANNarchy.intern.ConfigManagement import get_global_config, _update_global_config, _check_paradigm
 from ANNarchy.intern.GlobalObjects import GlobalObjectManager
@@ -750,7 +749,7 @@ def _instantiate(net_id, import_id=-1, cuda_config=None, user_config=None, core_
             if len(core_list) != len(list(set(core_list))):
                 Messages._warning("The provided core list contains doubled entries - is this intended?")
 
-            cython_module.set_number_threads(get_global_config('num_threads'), core_list)
+            cython_module.set_number_threads(get_global_config('num_threads'), np.array(core_list))
 
         else:
             # HD (26th Oct 2020): the current version of psutil only consider one CPU socket
@@ -765,7 +764,8 @@ def _instantiate(net_id, import_id=-1, cuda_config=None, user_config=None, core_
             # ANNarchy should run only on physical cpu cores
             core_list = np.arange(0, num_cores)
             """
-            cython_module.set_number_threads(get_global_config('num_threads'), [])
+            #cython_module.set_number_threads(get_global_config('num_threads'), np.array([]).astype(int))
+            pass
 
         if get_global_config('num_threads') > 1:
             if get_global_config('verbose'):
@@ -791,9 +791,9 @@ def _instantiate(net_id, import_id=-1, cuda_config=None, user_config=None, core_
     # Configure seeds for C++ random number generators
     # Required for state updates and also (in future) construction of connectivity
     if get_global_config('seed') is None:
-        seed = time.time()
+        seed = int(time.time())
     else:
-        seed = get_global_config('seed')
+        seed = int(get_global_config('seed'))
 
     if not get_global_config('disable_parallel_rng'):
         cython_module.set_seed(seed, get_global_config('num_threads'), get_global_config('use_seed_seq'))

ANNarchy/core/Monitor.py
@@ -395,7 +395,7 @@ class Monitor :
 
         def return_variable(self, name, keep):
             if isinstance(self.object, (Population, PopulationView)):
-                return reshape_recording(self, self._get_population(self.object, name, keep))
+                return reshape_recording(self, self._get_population(name, keep))
             elif isinstance(self.object, (Dendrite, Projection)):
                 data = self._get_dendrite(self.object, name, keep)
                 # Dendrites have one empty dimension
@@ -439,7 +439,7 @@ class Monitor :
         else:
             return data
 
-    def _get_population(self, pop, name, keep):
+    def _get_population(self, name, keep):
         try:
             data = getattr(self.cyInstance, name)
             if not keep:

ANNarchy/core/Population.py
@@ -208,7 +208,11 @@ class Population :
             t1 = time.time()
 
         try:
-            self.cyInstance = getattr(module, self.class_name+'_wrapper')(self.size, self.max_delay)
+            self.cyInstance = getattr(module, self.class_name+'_wrapper')()
+            self.cyInstance.size = self.size
+            self.cyInstance.max_delay = self.max_delay
+
+            print(self.class_name+'_wrapper', self.size, self.cyInstance.size)
         except:
             Messages._error('unable to instantiate the population', self.name)
 
@@ -221,6 +225,8 @@ class Population :
         # Initialize the population
         self.initialized = True
 
+        print(self.cyInstance, self.size, self.cyInstance.size)
+
         # Transfer the initial values of all attributes
         for name, value in self.init.items():
             if isinstance(value, Constant):
@@ -228,9 +234,8 @@ class Population :
             else:
                 self.__setattr__(name, value)
 
-
         # Activate the population
-        self.cyInstance.activate(self.enabled)
+        self.cyInstance.active = self.enabled
 
         # Reset to generate the right structures
         self.cyInstance.reset()
@@ -401,11 +406,17 @@ class Population :
             ctype = self._get_attribute_cpp_type(attribute)
             if attribute in self.neuron_type.description['local']:
                 if isinstance(value, np.ndarray):
-                    self.cyInstance.set_local_attribute_all(attribute, value.reshape(self.size), ctype)
+                    if ctype == "double":
+                        self.cyInstance.set_local_attribute_all_double(attribute, value.reshape(self.size))
+                    else:
+                        raise NotImplementedError
                 elif isinstance(value, list):
                     self.cyInstance.set_local_attribute_all(attribute, np.array(value).reshape(self.size), ctype)
                 else:
-                    self.cyInstance.set_local_attribute_all(attribute, value * np.ones( self.size ), ctype)
+                    if ctype == "double":
+                        self.cyInstance.set_local_attribute_all_double(attribute, value * np.ones( self.size ))
+                    else:
+                        raise NotImplementedError
             else:
                 self.cyInstance.set_global_attribute(attribute, value, ctype)
         except Exception as e:

ANNarchy/core/Projection.py
@@ -350,7 +350,8 @@ class Projection :
             if self._lil_connectivity:
                 return self.cyInstance.init_from_lil_connectivity(self._lil_connectivity)
             else:
-                return self.cyInstance.init_from_lil_connectivity(self._connection_method(*((self.pre, self.post,) + self._connection_args)))
+                synapses = self._connection_method(*((self.pre, self.post,) + self._connection_args))
+                return self.cyInstance.init_from_lil(synapses.post_rank, synapses.pre_rank, synapses.w, synapses.delay, synapses.requires_sorting)
 
         else:
             if get_global_config('verbose'):

ANNarchy/core/Simulate.py
@@ -4,15 +4,16 @@
 """
 
 from ANNarchy.intern.NetworkManager import NetworkManager
-from ANNarchy.core import Global
 from ANNarchy.core.Population import Population
+from ANNarchy.core import Global
 
 from ANNarchy.intern.Profiler import Profiler
+from ANNarchy.intern.ConfigManagement import get_global_config
 from ANNarchy.intern import Messages
-
 
 from math import ceil
 import time
+import tqdm
 import operator
 
 # Callbacks
@@ -48,7 +49,7 @@ def simulate(
         Messages._error('simulate(): the network is not compiled yet.')
 
     # Compute the number of steps
-    nb_steps = ceil(float(duration) / Global.dt())
+    nb_steps = ceil(float(duration) / get_global_config("dt"))
 
     if measure_time:
         tstart = time.time()
@@ -56,7 +57,24 @@ def simulate(
     if callbacks and _callbacks_enabled[net_id] and len(_callbacks[net_id]) > 0:
         _simulate_with_callbacks(duration, progress_bar, net_id)
     else:
-        NetworkManager().cy_instance(net_id=net_id).pyx_run(nb_steps, progress_bar)
+        batch = 1000
+        if nb_steps < batch:
+            NetworkManager().cy_instance(net_id=net_id).run(nb_steps)
+        else:
+            nb = int(nb_steps/batch)
+            rest = nb_steps % batch
+
+            if progress_bar:
+                pbar = tqdm(total=nb_steps*get_global_config("dt"))
+            for i in range(nb):
+                NetworkManager().cy_instance(net_id=net_id).run(batch)
+                if progress_bar:
+                    pbar.update(batch*get_global_config("dt"))
+            if progress_bar:
+                pbar.close()
+
+            if rest > 0:
+                NetworkManager().cy_instance(net_id=net_id).run(rest)
 
     if measure_time:
         if net_id > 0:
@@ -112,7 +130,7 @@ def simulate_until(max_duration:float, population: Population | list[Population]
     if not NetworkManager().cy_instance(net_id):
         Messages._error('simulate_until(): the network is not compiled yet.')
 
-    nb_steps = ceil(float(max_duration) / Global.dt())
+    nb_steps = ceil(float(max_duration) / get_global_config("dt"))
     if not isinstance(population, list):
         population = [population]
 
@@ -121,9 +139,9 @@ def simulate_until(max_duration:float, population: Population | list[Population]
 
     nb = NetworkManager().cy_instance(net_id).pyx_run_until(nb_steps, [pop.id for pop in population], True if operator=='and' else False)
 
-    sim_time = float(nb) / Global.dt()
+    sim_time = float(nb) / get_global_config("dt")
     if measure_time:
-        Messages._print('Simulating', nb/Global.dt()/1000.0, 'seconds of the network took', time.time() - tstart, 'seconds.')
+        Messages._print('Simulating', nb/get_global_config("dt")/1000.0, 'seconds of the network took', time.time() - tstart, 'seconds.')
     return sim_time
 
 

ANNarchy/generator/CodeGenerator.py
@@ -12,7 +12,7 @@ from ANNarchy.intern.GlobalObjects import GlobalObjectManager
 from ANNarchy.intern import Messages
 from ANNarchy.parser.Extraction import extract_functions
 
-from ANNarchy.generator.PyxGenerator import PyxGenerator
+from ANNarchy.generator.NanoBind.Generator import NanoBindGenerator
 from ANNarchy.generator.Monitor.MonitorGenerator import MonitorGenerator
 from ANNarchy.generator.Population import SingleThreadGenerator, OpenMPGenerator, CUDAGenerator
 from ANNarchy.generator.Projection import SingleThreadProjectionGenerator, OpenMPProjectionGenerator, CUDAProjectionGenerator
@@ -82,7 +82,7 @@ class CodeGenerator(object):
             Messages._error("No PopulationGenerator for " + get_global_config('paradigm'))
 
         # Py-extenstion and RecordGenerator are commonly defined
-        self._pyxgen = PyxGenerator(annarchy_dir, populations, projections, net_id)
+        self._nb_gen = NanoBindGenerator(annarchy_dir, populations, projections, net_id)
         self._recordgen = MonitorGenerator(annarchy_dir, populations, projections, net_id)
 
         # Target container for the generated code snippets
@@ -177,8 +177,8 @@ class CodeGenerator(object):
             raise NotImplementedError
 
         # Generate cython code for the analysed pops and projs
-        with open(source_dest+'ANNarchyCore'+str(self._net_id)+'.pyx', 'w') as ofile:
-            ofile.write(self._pyxgen.generate())
+        with open(source_dest+'ANNarchyCore'+str(self._net_id)+'.cpp', 'w') as ofile:
+            ofile.write(self._nb_gen.generate())
 
         self._generate_file_overview(source_dest)
 

ANNarchy/generator/Monitor/BaseTemplates.py
@@ -64,7 +64,7 @@ recording_spike_tpl= {
     'init' : """
         this->%(name)s = std::map<int,  std::vector< %(type)s > >();
         if(!this->partial){
-            for(int i=0; i<pop%(id)s.size; i++) {
+            for(int i=0; i<pop%(id)s->size; i++) {
                 this->%(name)s[i]=std::vector<%(type)s>();
             }
         }
@@ -78,25 +78,25 @@ recording_spike_tpl= {
     'record' : {
         'openmp' : """
         if(this->record_%(name)s){
-            for(int i=0; i<pop%(id)s.%(rec_target)s.size(); i++){
+            for(int i=0; i<pop%(id)s->%(rec_target)s.size(); i++){
                 if(!this->partial){
-                    this->%(name)s[pop%(id)s.%(rec_target)s[i]].push_back(t);
+                    this->%(name)s[pop%(id)s->%(rec_target)s[i]].push_back(t);
                 }
                 else{
-                    if( std::find(this->ranks.begin(), this->ranks.end(), pop%(id)s.%(rec_target)s[i])!=this->ranks.end() ){
-                        this->%(name)s[pop%(id)s.%(rec_target)s[i]].push_back(t);
+                    if( std::find(this->ranks.begin(), this->ranks.end(), pop%(id)s->%(rec_target)s[i])!=this->ranks.end() ){
+                        this->%(name)s[pop%(id)s->%(rec_target)s[i]].push_back(t);
                     }
                 }
             }
         } """,
         'cuda' : """if(this->record_spike){
-            for(int i=0; i<pop%(id)s.spike_count; i++){
+            for(int i=0; i<pop%(id)s->spike_count; i++){
                 if(!this->partial){
-                    this->spike[pop%(id)s.spiked[i]].push_back(t);
+                    this->spike[pop%(id)s->spiked[i]].push_back(t);
                 }
                 else{
-                    if( std::find(this->ranks.begin(), this->ranks.end(), pop%(id)s.spiked[i])!=this->ranks.end() ){
-                        this->spike[pop%(id)s.spiked[i]].push_back(t);
+                    if( std::find(this->ranks.begin(), this->ranks.end(), pop%(id)s->spiked[i])!=this->ranks.end() ){
+                        this->spike[pop%(id)s->spiked[i]].push_back(t);
                     }
                 }
             }
@@ -117,11 +117,13 @@ for ( auto it = %(name)s.begin(); it != %(name)s.end(); it++ ) {
     },
     'clear': {
         'openmp' : """
-            for (auto it = this->spike.begin(); it != this->spike.end(); it++) {
-                it->second.clear();
-                it->second.shrink_to_fit();
-            }
-            this->spike.clear();
+    void clear_spike() {
+        for (auto it = this->spike.begin(); it != this->spike.end(); it++) {
+            it->second.clear();
+            it->second.shrink_to_fit();
+        }
+        this->spike.clear();
+    }
         """,
         'cuda': """
         // TODO:

ANNarchy/generator/Monitor/MonitorGenerator.py
@@ -100,7 +100,8 @@ class MonitorGenerator(object):
         recording_target_code = ""
         struct_code = ""
         size_in_bytes = ""
-        clear_code = ""
+        clear_all_code = ""
+        clear_individual_code = ""
 
         # The post-synaptic potential for rate-code (weighted sum) as well
         # as the conductance variables are handled seperatly.
@@ -126,14 +127,16 @@ class MonitorGenerator(object):
                 struct_code += template['local']['struct'] % tar_dict
                 init_code += template['local']['init'] % tar_dict
                 recording_target_code += template['local']['recording'] % tar_dict
-                clear_code += template['local']['clear'] % tar_dict
+                clear_all_code += "\t\tthis->clear_{name}();\n".format(name='_sum_'+target)
+                clear_individual_code += template['local']['clear'] % tar_dict
         else:
             for target in targets:
                 tar_dict = {'id': pop.id, 'type' : get_global_config('precision'), 'name': 'g_'+target}
                 struct_code += template['local']['struct'] % tar_dict
                 init_code += template['local']['init'] % tar_dict
                 recording_target_code += template['local']['recording'] % tar_dict
-                clear_code += template['local']['clear'] % tar_dict
+                clear_all_code += "\t\tthis->clear_{name}();\n".format(name='g_'+target)
+                clear_individual_code += template['local']['clear'] % tar_dict
 
                 # to skip this entry in the following loop
                 target_list.append('g_'+target)
@@ -158,7 +161,8 @@ class MonitorGenerator(object):
             struct_code += template[var['locality']]['struct'] % ids
             init_code += template[var['locality']]['init'] % ids
             recording_code += template[var['locality']]['recording'] % ids
-            clear_code += template[var['locality']]['clear'] % ids
+            clear_all_code += "\t\tthis->clear_{name}();\n".format(name=ids['name'])
+            clear_individual_code += template[var['locality']]['clear'] % ids
             size_in_bytes += template[var['locality']]['size_in_bytes'] % ids
 
         # Record spike events
@@ -175,7 +179,7 @@ class MonitorGenerator(object):
             init_code += base_tpl['init'] % rec_dict
             recording_code += base_tpl['record'][get_global_config('paradigm')] % rec_dict
             size_in_bytes += base_tpl['size_in_bytes'][get_global_config('paradigm')] % rec_dict
-            clear_code += base_tpl['clear'][get_global_config('paradigm')] % rec_dict
+            clear_all_code += "\t\tthis->clear_spike();\n"
 
             # Record axon spike events
             if pop.neuron_type.axon_spike:
@@ -197,7 +201,8 @@ class MonitorGenerator(object):
             'recording_code': recording_code,
             'recording_target_code': recording_target_code,
             'size_in_bytes': tabify(size_in_bytes, 2),
-            'clear_monitor_code': clear_code
+            'clear_all_container_code': clear_all_code,
+            'clear_individual_container_code': clear_individual_code
         }
         return tpl_code % ids
 

commit f81e2dd7ecb86c0a8a74a9608862165e89e85050

Fixed an error introduced in commit bdc4dd1, which left the number of threads unitialized crashing the simulation.

ANNarchy/generator/Compiler.py
@@ -764,8 +764,7 @@ def _instantiate(net_id, import_id=-1, cuda_config=None, user_config=None, core_
             # ANNarchy should run only on physical cpu cores
             core_list = np.arange(0, num_cores)
             """
-            #cython_module.set_number_threads(get_global_config('num_threads'), np.array([]).astype(int))
-            pass
+            cython_module.set_number_threads(get_global_config('num_threads'), [])
 
         if get_global_config('num_threads') > 1:
             if get_global_config('verbose'):