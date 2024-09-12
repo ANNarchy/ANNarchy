93c3ff8522f11ebe00aee5a07d1d45f63d161eaf
Helge Dinkelbach <helge.dinkelbach@gmail.com>
2024-09-12 16:54:41 +0200

    Removed unnecessary files.

--- tests/coverage_check.sh
+++
@@ -1,5 +0,0 @@
-coverage erase
-coverage run --source /scratch/alexschw/annarchy_install/lib/python3.6/site-packages/ANNarchy/ -a test_single_thread.py
-coverage run --source /scratch/alexschw/annarchy_install/lib/python3.6/site-packages/ANNarchy/ -a test_openmp.py
-coverage run --source /scratch/alexschw/annarchy_install/lib/python3.6/site-packages/ANNarchy/ -a test_CUDA.py
-coverage report
