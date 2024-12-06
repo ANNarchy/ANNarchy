6c241489dfcdfdee4ab8c0cb3f6b39457fcad51d
Julien Vitay <julien.vitay@informatik.tu-chemnitz.de>
2024-12-06 12:20:41 +0100

    Very minor release 4.8.2.2

--- ANNarchy/__init__.py
+++ ANNarchy/__init__.py
@@ -51,7 +51,7 @@ atexit.register(clear)
 
 # Version
 __version__ = '4.8'
-__release__ = '4.8.2'
+__release__ = '4.8.2.2'
 
 print( 'ANNarchy ' + __version__ + ' (' + __release__ + \
                     ') on ' + sys.platform + ' (' + os.name + ').' )
