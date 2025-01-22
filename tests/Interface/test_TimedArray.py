71b8d82c2c749498322dea8ea2053a2c33390050
Helge Dinkelbach <helge.dinkelbach@gmail.com>
2025-01-22 10:23:29 +0100

    Adapted TimedArray unittests and an error is thrown: set_schedule function is missing in the nanobind interface.

--- tests/Interface/test_TimedArray.py
+++ tests/Interface/test_TimedArray.py
@@ -22,8 +22,9 @@
 
 """
 import unittest
+import numpy
 
-from ANNarchy import *
+from ANNarchy import Network, Neuron, TimedArray
 
 class test_TimedArray(unittest.TestCase):
     """
@@ -35,7 +36,7 @@ class test_TimedArray(unittest.TestCase):
         Compile the network for this test. Adapted the example
         from documentation ( section 3.7.3 Class TimedArray )
         """
-        inputs = np.array(
+        inputs = numpy.array(
             [
                 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                 [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -58,41 +59,39 @@ class test_TimedArray(unittest.TestCase):
                 r4 = sum(exc4)
             """
         )
-        inp = TimedArray(rates=inputs)
-        inp2 = TimedArray(rates=inputs, schedule=10.)
-        inp3 = TimedArray(rates=inputs, period=10.)
-        inp4 = TimedArray(rates=inputs, schedule=2., period=20.)
 
-        pop = Population(10, neuron=SimpleNeuron)
+        cls._network = Network()
 
-        proj = Projection(inp, pop, 'exc')
+        inp = cls._network.create(geometry=10, population=TimedArray(rates=inputs))
+        inp2 = cls._network.create(geometry=10, population=TimedArray(rates=inputs, schedule=10.))
+        inp3 = cls._network.create(geometry=10, population=TimedArray(rates=inputs, period=10.))
+        inp4 = cls._network.create(geometry=10, population=TimedArray(rates=inputs, schedule=2., period=20.))
+
+        cls.output = cls._network.create(10, neuron=SimpleNeuron)
+
+        proj = cls._network.connect(inp, cls.output, 'exc')
         proj.connect_one_to_one(1.0)
-        proj2 = Projection(inp2, pop, 'exc2')
+        proj2 = cls._network.connect(inp2, cls.output, 'exc2')
         proj2.connect_one_to_one(1.0)
-        proj3 = Projection(inp3, pop, 'exc3')
+        proj3 = cls._network.connect(inp3, cls.output, 'exc3')
         proj3.connect_one_to_one(1.0)
-        proj4 = Projection(inp4, pop, 'exc4')
+        proj4 = cls._network.connect(inp4, cls.output, 'exc4')
         proj4.connect_one_to_one(1.0)
 
-        cls.test_net = Network()
-        cls.test_net.add([inp, inp2, inp3, inp4, pop, proj, proj2, proj3, proj4])
-        cls.test_net.compile(silent=True)
-
-        cls.output = cls.test_net.get(pop)
+        cls._network.compile(silent=True)
 
     @classmethod
     def tearDownClass(cls):
         """
         All tests of this class are done. We can destroy the network.
         """
-        del cls.test_net
-        clear()
+        del cls._network
 
     def setUp(self):
         """
         Automatically called before each test method, basically to reset the network after every test.
         """
-        self.test_net.reset()
+        self._network.reset()
 
     def test_compile(self):
         """
@@ -104,43 +103,43 @@ class test_TimedArray(unittest.TestCase):
         """
         We provide 10 ms input data, after 11ms the last element will be set.
         """
-        self.test_net.simulate(11)
-        np.testing.assert_allclose(self.output.r, [0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
+        self._network.simulate(11)
+        numpy.testing.assert_allclose(self.output.r, [0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
 
     def test_more_steps(self):
         """
         We provide 10 ms input data, after 11ms the last element will be remain in the buffer.
         """
-        self.test_net.simulate(12)
-        np.testing.assert_allclose(self.output.r, [0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
+        self._network.simulate(12)
+        numpy.testing.assert_allclose(self.output.r, [0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
 
     def test_run_one_schedule(self):
         """
         We provide 10 ms input data the 1st entry, after 11ms the 2nd element will be set. So the next 10
         iterations should return the 2nd element of the input array.
         """
-        self.test_net.simulate(11) # 1st entry
+        self._network.simulate(11) # 1st entry
 
-        self.test_net.simulate(10) # 2nd entry
-        np.testing.assert_allclose(self.output.r2, [0, 1, 0, 0, 0, 0, 0, 0, 0, 0])
+        self._network.simulate(10) # 2nd entry
+        numpy.testing.assert_allclose(self.output.r2, [0, 1, 0, 0, 0, 0, 0, 0, 0, 0])
 
     def test_run_one_period(self):
         """
         Period is set to 10, this means after 11 ms we have one cycle complete. This means the 12 step
         should return the 1st element in the output population.
         """
-        self.test_net.simulate(12) # 1st entry
+        self._network.simulate(12) # 1st entry
 
-        np.testing.assert_allclose(self.output.r3, [1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        numpy.testing.assert_allclose(self.output.r3, [1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
     def test_run_one_period_one_schedule(self):
         """
         Schedule == 2 doubles all entries. Period of 20 is one complete iteration. This means
         after 25 steps we are again on the 2nd entry.
         """
-        self.test_net.simulate(25)
+        self._network.simulate(25)
 
-        np.testing.assert_allclose(self.output.r4, [0, 1, 0, 0, 0, 0, 0, 0, 0, 0])
+        numpy.testing.assert_allclose(self.output.r4, [0, 1, 0, 0, 0, 0, 0, 0, 0, 0])
 
 class test_TimedArrayLateInit(unittest.TestCase):
     """
@@ -152,7 +151,7 @@ class test_TimedArrayLateInit(unittest.TestCase):
         Compile the network for this test. Adapted the example
         from documentation ( section 3.7.3 Class TimedArray )
         """
-        inputs = np.array(
+        inputs = numpy.array(
             [
                 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                 [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -176,46 +175,46 @@ class test_TimedArrayLateInit(unittest.TestCase):
             """
         )
 
-        inp = TimedArray(geometry=(10))
-        inp2 = TimedArray(geometry=(10))
-        inp3 = TimedArray(geometry=(10))
-        inp4 = TimedArray(geometry=(10))
+        cls._network = Network()
 
+        # Create TimedArrays without a pre-defined *rates* array
+        inp = cls._network.create(geometry=10, population=TimedArray(geometry=(10)))
+        inp2 = cls._network.create(geometry=10, population=TimedArray(geometry=(10)))
+        inp3 = cls._network.create(geometry=10, population=TimedArray(geometry=(10)))
+        inp4 = cls._network.create(geometry=10, population=TimedArray(geometry=(10)))
+
+        # Test later initialization
         inp.update(rates=inputs)
         inp2.update(rates=inputs, schedule=10.)
         inp3.update(rates=inputs, period=10.)
         inp4.update(rates=inputs, schedule=2., period=20.)
 
-        pop = Population(10, neuron=SimpleNeuron)
+        # The output signal will be tested
+        cls.output = cls._network.create(10, neuron=SimpleNeuron)
 
-        proj = Projection(inp, pop, 'exc')
+        proj = cls._network.connect(inp, cls.output, 'exc')
         proj.connect_one_to_one(1.0)
-        proj2 = Projection(inp2, pop, 'exc2')
+        proj2 = cls._network.connect(inp2, cls.output, 'exc2')
         proj2.connect_one_to_one(1.0)
-        proj3 = Projection(inp3, pop, 'exc3')
+        proj3 = cls._network.connect(inp3, cls.output, 'exc3')
         proj3.connect_one_to_one(1.0)
-        proj4 = Projection(inp4, pop, 'exc4')
+        proj4 = cls._network.connect(inp4, cls.output, 'exc4')
         proj4.connect_one_to_one(1.0)
 
-        cls.test_net = Network()
-        cls.test_net.add([inp, inp2, inp3, inp4, pop, proj, proj2, proj3, proj4])
-        cls.test_net.compile(silent=True)
-
-        cls.output = cls.test_net.get(pop)
+        cls._network.compile(silent=True)
 
     @classmethod
     def tearDownClass(cls):
         """
         All tests of this class are done. We can destroy the network.
         """
-        del cls.test_net
-        clear()
+        del cls._network
 
     def setUp(self):
         """
         Automatically called before each test method, basically to reset the network after every test.
         """
-        self.test_net.reset()
+        self._network.reset()
 
     def test_compile(self):
         """
@@ -227,43 +226,43 @@ class test_TimedArrayLateInit(unittest.TestCase):
         """
         We provide 10 ms input data, after 11ms the last element will be set.
         """
-        self.test_net.simulate(11)
-        np.testing.assert_allclose(self.output.r, [0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
+        self._network.simulate(11)
+        numpy.testing.assert_allclose(self.output.r, [0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
 
     def test_more_steps(self):
         """
         We provide 10 ms input data, after 11ms the last element will be remain in the buffer.
         """
-        self.test_net.simulate(12)
-        np.testing.assert_allclose(self.output.r, [0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
+        self._network.simulate(12)
+        numpy.testing.assert_allclose(self.output.r, [0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
 
     def test_run_one_schedule(self):
         """
         We provide 10 ms input data the 1st entry, after 11ms the 2nd element will be set. So the next 10
         iterations should return the 2nd element of the input array.
         """
-        self.test_net.simulate(11) # 1st entry
+        self._network.simulate(11) # 1st entry
 
-        self.test_net.simulate(10) # 2nd entry
-        np.testing.assert_allclose(self.output.r2, [0, 1, 0, 0, 0, 0, 0, 0, 0, 0])
+        self._network.simulate(10) # 2nd entry
+        numpy.testing.assert_allclose(self.output.r2, [0, 1, 0, 0, 0, 0, 0, 0, 0, 0])
 
     def test_run_one_period(self):
         """
         Period is set to 10, this means after 11 ms we have one cycle complete. This means the 12 step
         should return the 1st element in the output population.
         """
-        self.test_net.simulate(12) # 1st entry
+        self._network.simulate(12) # 1st entry
 
-        np.testing.assert_allclose(self.output.r3, [1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        numpy.testing.assert_allclose(self.output.r3, [1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
     def test_run_one_period_one_schedule(self):
         """
         Schedule == 2 doubles all entries. Period of 20 is one complete iteration. This means
         after 25 steps we are again on the 2nd entry.
         """
-        self.test_net.simulate(25)
+        self._network.simulate(25)
 
-        np.testing.assert_allclose(self.output.r4, [0, 1, 0, 0, 0, 0, 0, 0, 0, 0])
+        numpy.testing.assert_allclose(self.output.r4, [0, 1, 0, 0, 0, 0, 0, 0, 0, 0])
 
 class test_TimedArrayUpdate(unittest.TestCase):
     """
@@ -272,7 +271,7 @@ class test_TimedArrayUpdate(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.initial_inputs = np.array(
+        cls.initial_inputs = numpy.array(
             [
                 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                 [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -287,7 +286,7 @@ class test_TimedArrayUpdate(unittest.TestCase):
             ]
         )
 
-        cls.overwrite_inputs = np.array(
+        cls.overwrite_inputs = numpy.array(
             [
                 [1, 0, 0, 0, 0, 0, 0, 0, 0, 2],
                 [0, 1, 0, 0, 0, 0, 0, 0, 2, 0],
@@ -307,38 +306,34 @@ class test_TimedArrayUpdate(unittest.TestCase):
                 r = sum(exc_1) + sum(exc_2)
             """
         )
-        inp = TimedArray(rates=cls.initial_inputs)
-        inp2 = TimedArray(rates=cls.initial_inputs, schedule=2.)
 
-        pop = Population(10, neuron=SimpleNeuron)
+        cls._network = Network()
 
-        proj1 = Projection(inp, pop, 'exc_1')
+        cls.input = cls._network.create(geometry=10, population=TimedArray(rates=cls.initial_inputs))
+        cls.input2 = cls._network.create(geometry=10, population=TimedArray(rates=cls.initial_inputs, schedule=2.))
+
+        cls.output = cls._network.create(geometry=10, neuron=SimpleNeuron)
+
+        proj1 = cls._network.connect(cls.input, cls.output, 'exc_1')
         proj1.connect_one_to_one(1.0)
 
-        proj2 = Projection(inp2, pop, 'exc_2')
+        proj2 = cls._network.connect(cls.input2, cls.output, 'exc_2')
         proj2.connect_one_to_one(1.0)
 
-        cls.test_net = Network()
-        cls.test_net.add([inp, inp2, pop, proj1, proj2])
-        cls.test_net.compile(silent=True)
-
-        cls.input = cls.test_net.get(inp)
-        cls.input2 = cls.test_net.get(inp2)
-        cls.output = cls.test_net.get(pop)
+        cls._network.compile(silent=True)
 
     @classmethod
     def tearDownClass(cls):
         """
         All tests of this class are done. We can destroy the network.
         """
-        del cls.test_net
-        clear()
+        del cls._network
 
     def setUp(self):
         """
         Automatically called before each test method, basically to reset the network after every test.
         """
-        self.test_net.reset()
+        self._network.reset()
 
     def test_compile(self):
         """
@@ -351,18 +346,18 @@ class test_TimedArrayUpdate(unittest.TestCase):
         We provide 5 ms input data, then we set another input sequence and simulate 6 ms more. The
         expected result is the last value in the input buffer, as cycling is disabled.
         """
-        self.test_net.simulate(5)
+        self._network.simulate(5)
         self.input.update(self.overwrite_inputs)
-        self.test_net.simulate(6)
-        np.testing.assert_allclose(self.output.sum("exc_1"), [2, 0, 0, 0, 0, 0, 0, 0, 0, 1])
+        self._network.simulate(6)
+        numpy.testing.assert_allclose(self.output.sum("exc_1"), [2, 0, 0, 0, 0, 0, 0, 0, 0, 1])
 
     def test_run_one_loop_and_period(self):
         """
         We provide 5 ms input data, then we set another input sequence and simulate 6 ms more. As we
         have a schedule of 2 ms, the 5-th position should be read out.
         """
-        self.test_net.simulate(5)
+        self._network.simulate(5)
         self.input2.update(self.overwrite_inputs, schedule=2.0)
-        self.test_net.simulate(6)
-        np.testing.assert_allclose(self.output.sum("exc_2"), [0, 0, 0, 0, 1, 2, 0, 0, 0, 0])
+        self._network.simulate(6)
+        numpy.testing.assert_allclose(self.output.sum("exc_2"), [0, 0, 0, 0, 1, 2, 0, 0, 0, 0])
 
