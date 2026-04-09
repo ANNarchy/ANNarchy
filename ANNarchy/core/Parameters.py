48de472545142cdccd5e086e735cf5d8d8b6a3f4
Helge Dinkelbach <helge.dinkelbach@gmail.com>
2026-04-09 16:04:06 +0200

    attributes created with *Parameter* class should be global by default too.

--- ANNarchy/core/Parameters.py
+++ ANNarchy/core/Parameters.py
@@ -18,11 +18,11 @@ class Parameter:
     neuron = ann.Neuron(
         parameters = dict(
 
-            # Global parameter
-            tau = 10.0 # or ann.Parameter(value=10.0, locality='global')
+            # Global parameter (default)
+            tau = 10.0 # or ann.Parameter(value=10.0),
 
             # Local parameter
-            baseline = ann.Parameter(value=ann.Uniform(-1., 1.)),
+            baseline = ann.Parameter(value=ann.Uniform(-1., 1.), locality='local'),
 
             # Boolean global parameter
             activated = ann.Parameter(value=True, locality='global', type=bool),
@@ -30,11 +30,11 @@ class Parameter:
     )
     ```
 
-    In a neuron or synapse model, parameters are global and use the float type if the `Parameter` class is not used.
+    In a neuron or synapse model, parameters defined as direct assignment are by default `global` and use the float type.
 
-    If you need a local parameter (one value per neuron or synapse), the `Parameter` class allows to specify it. Note that you can also define global parameters by passing `locality='global'`.
+    If you need a local parameter (one value per neuron or synapse), the `Parameter` class allows to specify it using `locality='local'`.
 
-    Semi-global synaptic parameters (one value per post-synaptic neuron) can be defined using `locality='semiglobalglobal'`.
+    Semi-global synaptic parameters (one value per post-synaptic neuron) can be defined using `locality='semiglobal'`.
 
     If the parameter is an int or a bool, pass it to the `type` attribute.
 
@@ -44,7 +44,7 @@ class Parameter:
     """
 
     value: float | int | bool | RandomDistribution
-    locality: str = "local"
+    locality: str = "global"
     type: str = "float"
 
 
