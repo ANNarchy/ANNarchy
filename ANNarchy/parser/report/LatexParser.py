ANNarchy/ANNarchy

commit f9ad29f8611a385197ffd5e14c20020a061492f6

refactor: removed Messages._print() method and replace by default print(). I suspect that this is a relic from the time when Python 2.x/3.x were used at the same time ... (contributes to issue #40)

ANNarchy/parser/report/LatexParser.py
@@ -368,7 +368,7 @@ def _analyse_equation(orig, eq, local_dict, tex_dict):
         try:
             left = _analyse_part(left[:-1], local_dict, tex_dict)
         except Exception as e:
-            Messages._print(e)
+            print(e)
             Messages.warning(
                 "can not transform the left side of "
                 + orig
@@ -381,7 +381,7 @@ def _analyse_equation(orig, eq, local_dict, tex_dict):
         try:
             left = _analyse_part(left, local_dict, tex_dict)
         except Exception as e:
-            Messages._print(e)
+            print(e)
             Messages.warning(
                 "can not transform the left side of "
                 + orig
@@ -393,7 +393,7 @@ def _analyse_equation(orig, eq, local_dict, tex_dict):
     try:
         right = _analyse_part(eq[split_idx + 1 :], local_dict, tex_dict)
     except Exception as e:
-        Messages._print(e)
+        print(e)
         Messages.warning(
             "can not transform the right side of "
             + orig
@@ -410,7 +410,7 @@ class CustomLatexPrinter(LatexPrinter):
         For ite(), pos() and neg() only.
         """
         func = expr.func.__name__
-        args = [str(self._print(arg)) for arg in expr.args]
+        args = [str(print(arg)) for arg in expr.args]
 
         if func == "ite":
             return r"""

ANNarchy/core/ConnectorMethods.py
@@ -18,7 +18,7 @@ from ANNarchy.intern.ConfigManagement import ConfigManager
 try:
     from ANNarchy.cython_ext import *
 except Exception as e:
-    Messages._print(e)
+    print(e)
 
 
 ################################
@@ -48,7 +48,7 @@ def connect_one_to_one(
             "with target",
             self.target,
         )
-        Messages._print(
+        print(
             "\t the two populations have different sizes, please check the connection pattern is what you expect."
         )
 
@@ -672,18 +672,18 @@ def _load_from_matrix(self, pre, post, weights, delays, pre_post):
     shape = weights.shape
     if shape != (self.post.size, self.pre.size):
         if not pre_post:
-            Messages._print(
+            print(
                 "ERROR: from_matrix(): the matrix does not have the correct dimensions."
             )
-            Messages._print("Expected:", (self.post.size, self.pre.size))
-            Messages._print("Received:", shape)
+            print("Expected:", (self.post.size, self.pre.size))
+            print("Received:", shape)
 
         else:
-            Messages._print(
+            print(
                 "ERROR: from_matrix(): the matrix does not have the correct dimensions."
             )
-            Messages._print("Expected:", (self.pre.size, self.post.size))
-            Messages._print("Received:", shape)
+            print("Expected:", (self.pre.size, self.post.size))
+            print("Received:", shape)
         Messages._error("Quitting...")
 
     for i in range(self.post.size):
@@ -780,11 +780,11 @@ def _load_from_sparse(self, pre, post, weights, delays):
     (pre, post) = weights.shape
 
     if (pre, post) != (len(pre_ranks), len(post_ranks)):
-        Messages._print(
+        print(
             "ERROR: from_sparse(): the sparse matrix does not have the correct dimensions."
         )
-        Messages._print("Expected:", (len(pre_ranks), len(post_ranks)))
-        Messages._print("Received:", (pre, post))
+        print("Expected:", (len(pre_ranks), len(post_ranks)))
+        print("Received:", (pre, post))
         Messages._error("Quitting...")
 
     for idx_post in range(post):
@@ -821,7 +821,7 @@ def connect_from_file(
     try:
         data = _load_connectivity_data(filename, pickle_encoding)
     except Exception as e:
-        Messages._print(e)
+        print(e)
         Messages._error(
             "from_file(): Unable to load the data", filename, "into the projection."
         )
@@ -850,13 +850,13 @@ def connect_from_file(
         if NetworkManager().get_network(net_id=self.net_id).compiled:
             # We have already compiled the network, so changing the flag will result in crashes when accessing 'w'
             if single_w != self._single_constant_weight:
-                Messages._print(
+                print(
                     "Projection (name="
                     + self.name
                     + "): potential mismatch between weight vector in the projection and the save file."
                 )
-                Messages._print("    single weight in file:", single_w)
-                Messages._print(
+                print("    single weight in file:", single_w)
+                print(
                     "    single weight in network:", self._single_constant_weight
                 )
         else:
@@ -876,7 +876,7 @@ def connect_from_file(
                 lil.delay = [[lil.max_delay]]
 
     except Exception as e:
-        Messages._print(e)
+        print(e)
         Messages._error("Unable to load the data", filename, "into the projection.")
 
     # Store the synapses

ANNarchy/core/Dendrite.py
@@ -337,7 +337,7 @@ class Dendrite:
                 *extra_attributes,
             )
         except Exception as e:
-            Messages._print(e)
+            print(e)
             Messages._error(
                 f"Dendrite.create_synapse(): Could not add synapse of rank {rank} to the dendrite {self.post_rank}."
             )
@@ -437,7 +437,7 @@ class Dendrite:
                 post_idx, ranks, weights, delays, *extra_attributes
             )
         except Exception as e:
-            Messages._print(e)
+            print(e)
 
     def prune_synapse(self, rank: int) -> None:
         """

ANNarchy/core/IO.py
@@ -52,8 +52,8 @@ def load_parameters(
         Messages._error("load_parameters(): the json file does not exist")
 
     if verbose:
-        Messages._print("Loading parameters from file", filename)
-        Messages._print("-" * 40)
+        print("Loading parameters from file", filename)
+        print("-" * 40)
 
     # Populations
     try:
@@ -61,7 +61,7 @@ def load_parameters(
     except:
         populations = {}
         if verbose:
-            Messages._print("load_parameters(): no population parameters.")
+            print("load_parameters(): no population parameters.")
     for name, parameters in populations.items():
         # Get the population
         for pop in NetworkManager().get_network(net_id=net_id).get_populations():
@@ -78,13 +78,13 @@ def load_parameters(
             )
 
         if verbose:
-            Messages._print("Population", name)
+            print("Population", name)
 
         # Set the parameters
         for name, val in parameters.items():
             # Check that the variable indeed exists
             if not name in population.parameters:
-                Messages._print(
+                print(
                     "  ",
                     name,
                     "is not a global parameter of",
@@ -93,7 +93,7 @@ def load_parameters(
                 )
                 continue
             if global_only and not name in population.neuron_type.description["global"]:
-                Messages._print(
+                print(
                     "  ",
                     name,
                     "is not a global parameter of",
@@ -103,7 +103,7 @@ def load_parameters(
                 continue
 
             if verbose:
-                Messages._print("  ", name, ":", population.get(name), "->", val)
+                print("  ", name, ":", population.get(name), "->", val)
 
             population.set({name: val})
 
@@ -113,7 +113,7 @@ def load_parameters(
     except:
         projections = {}
         if verbose:
-            Messages._print("load_parameters(): no projection parameters.")
+            print("load_parameters(): no projection parameters.")
     for name, parameters in projections.items():
         # Get the projection
         for proj in NetworkManager().get_network(net_id=net_id).get_projections():
@@ -130,13 +130,13 @@ def load_parameters(
             )
 
         if verbose:
-            Messages._print("Projection", name)
+            print("Projection", name)
 
         # Set the parameters
         for name, val in parameters.items():
             # Check that the variable indeed exists
             if not name in projection.parameters:
-                Messages._print(
+                print(
                     "  ",
                     name,
                     "is not a global parameter of",
@@ -148,7 +148,7 @@ def load_parameters(
                 global_only
                 and not name in projection.synapse_type.description["global"]
             ):
-                Messages._print(
+                print(
                     "  ",
                     name,
                     "is not a global parameter of",
@@ -158,7 +158,7 @@ def load_parameters(
                 continue
 
             if verbose:
-                Messages._print("  ", name, ":", projection.get(name), "->", val)
+                print("  ", name, ":", projection.get(name), "->", val)
 
             projection.set({name: float(val)})
 
@@ -168,7 +168,7 @@ def load_parameters(
     except:
         constants = {}
         if verbose:
-            Messages._print("load_parameters(): no constants.")
+            print("load_parameters(): no constants.")
     for name, value in constants.items():
         # Get the constant
         for constant in NetworkManager().get_network(net_id=net_id).get_constants():
@@ -271,7 +271,7 @@ def _load_parameters_from_xml(in_file):
     try:
         from lxml import etree
     except:
-        Messages._print("lxml is not installed. Unable to load in xml format.")
+        print("lxml is not installed. Unable to load in xml format.")
         return
     par = {}
     damaged_pars = []  # for printout
@@ -287,7 +287,7 @@ def _load_parameters_from_xml(in_file):
             doc = etree.parse(file)
 
         except IOError:
-            Messages._print("Error: file '", file, "' not found.")
+            print("Error: file '", file, "' not found.")
             continue
 
         matches = doc.findall("parameter")
@@ -297,7 +297,7 @@ def _load_parameters_from_xml(in_file):
 
             # TODO: allways correct ???
             if len(childs) != 2:
-                Messages._print("Error: to much tags in parameter")
+                print("Error: to much tags in parameter")
 
             name = None
             value = None
@@ -308,7 +308,7 @@ def _load_parameters_from_xml(in_file):
                     value = child.text
 
                     if value is None:
-                        Messages._print("Error: no value defined for", name)
+                        print("Error: no value defined for", name)
                         damaged_pars.append(name)
                         value = 0
                     else:
@@ -321,15 +321,15 @@ def _load_parameters_from_xml(in_file):
                                 value = value
 
                 else:
-                    Messages._print("Error: unexpected xml-tag", child.tag)
+                    print("Error: unexpected xml-tag", child.tag)
 
             if name is None:
-                Messages._print("Error: no name in parameter set.")
+                print("Error: no name in parameter set.")
             elif value is None:
-                Messages._print("Error: no value in parameter set.")
+                print("Error: no value in parameter set.")
                 damaged_pars.append(name)
             elif name in par.keys():
-                Messages._print("Error: parameter", name, "already exists.")
+                print("Error: parameter", name, "already exists.")
                 damaged_pars.append(name)
             else:
                 par[name] = value
@@ -347,24 +347,24 @@ def _save_data(filename, data):
 
     if not path == "":
         if not os.path.isdir(path):
-            Messages._print("Creating folder", path)
+            print("Creating folder", path)
             os.mkdir(path)
 
     extension = os.path.splitext(fname)[1]
 
     if extension == ".mat":
-        Messages._print("Saving network in Matlab format...")
+        print("Saving network in Matlab format...")
         try:
             import scipy.io as sio
 
             sio.savemat(filename, data)
         except Exception as e:
             Messages._error("Error while saving in Matlab format.")
-            Messages._print(e)
+            print(e)
             return
 
     elif extension == ".gz":
-        Messages._print("Saving network in gunzipped binary format...")
+        print("Saving network in gunzipped binary format...")
         try:
             import gzip
         except:
@@ -374,23 +374,23 @@ def _save_data(filename, data):
             try:
                 pickle.dump(data, w_file, protocol=pickle.HIGHEST_PROTOCOL)
             except Exception as e:
-                Messages._print("Error while saving in gzipped binary format.")
-                Messages._print(e)
+                print("Error while saving in gzipped binary format.")
+                print(e)
                 return
 
     elif extension == ".npz":
-        Messages._print("Saving network in Numpy format...")
+        print("Saving network in Numpy format...")
         np.savez_compressed(filename, allow_pickle=True, **data)
 
     else:
-        Messages._print("Saving network in text format...")
+        print("Saving network in text format...")
         # save in Pythons pickle format
         with open(filename, mode="wb") as w_file:
             try:
                 pickle.dump(data, w_file, protocol=pickle.HIGHEST_PROTOCOL)
             except Exception as e:
-                Messages._print("Error while saving in text format.")
-                Messages._print(e)
+                print("Error while saving in text format.")
+                print(e)
                 return
         return
 
@@ -461,8 +461,8 @@ def _load_data(filename, pickle_encoding):
                     desc = pickle.load(r_file, encoding=pickle_encoding)
             return desc
         except Exception as e:
-            Messages._print("Unable to read the file " + filename)
-            Messages._print(e)
+            print("Unable to read the file " + filename)
+            print(e)
             return None
 
     elif extension == ".npz":
@@ -486,8 +486,8 @@ def _load_data(filename, pickle_encoding):
 
             return desc
         except Exception as e:
-            Messages._print("Unable to read the file " + filename)
-            Messages._print(e)
+            print("Unable to read the file " + filename)
+            print(e)
             return None
 
     else:
@@ -499,8 +499,8 @@ def _load_data(filename, pickle_encoding):
                     desc = pickle.load(r_file, encoding=pickle_encoding)
             return desc
         except Exception as e:
-            Messages._print("Unable to read the file " + filename)
-            Messages._print(e)
+            print("Unable to read the file " + filename)
+            print(e)
             return None
 
 
@@ -534,8 +534,8 @@ def _load_connectivity_data(filename, pickle_encoding):
                     desc = pickle.load(r_file, encoding=pickle_encoding)
             return desc
         except Exception as e:
-            Messages._print("Unable to read the file " + filename)
-            Messages._print(e)
+            print("Unable to read the file " + filename)
+            print(e)
             return None
 
     elif extension == ".npz":
@@ -554,8 +554,8 @@ def _load_connectivity_data(filename, pickle_encoding):
 
             return desc
         except Exception as e:
-            Messages._print("Unable to read the file " + filename)
-            Messages._print(e)
+            print("Unable to read the file " + filename)
+            print(e)
             return None
 
     else:
@@ -567,8 +567,8 @@ def _load_connectivity_data(filename, pickle_encoding):
                     desc = pickle.load(r_file, encoding=pickle_encoding)
             return desc
         except Exception as e:
-            Messages._print("Unable to read the file " + filename)
-            Messages._print(e)
+            print("Unable to read the file " + filename)
+            print(e)
             return None
 
 

ANNarchy/core/Monitor.py
@@ -425,7 +425,7 @@ class Monitor:
                         + self.object.proj.post.name
                     )
                     if var in self.object.proj.parameters:
-                        Messages._print(
+                        print(
                             "\t", var, "is a parameter, its value is constant"
                         )
 

ANNarchy/core/Network.py
@@ -882,7 +882,7 @@ class Network(metaclass=NetworkMeta):
 
         # Time measurement
         if measure_time:
-            Messages._print(
+            print(
                 "Simulating",
                 number,
                 "networks in parallel took",
@@ -1221,7 +1221,7 @@ class Network(metaclass=NetworkMeta):
         for pop in self._data.populations:
             if pop.name == name:
                 return pop
-        Messages._print(
+        print(
             "get_population(): the population", name, "does not exist in this network."
         )
         return None
@@ -1235,7 +1235,7 @@ class Network(metaclass=NetworkMeta):
         for proj in self._data.projections:
             if proj.name == name:
                 return proj
-        Messages._print(
+        print(
             "get_projection(): the projection", name, "does not exist in this network."
         )
         return None
@@ -1249,7 +1249,7 @@ class Network(metaclass=NetworkMeta):
         for mon in self._data.monitors:
             if mon.name == name:
                 return mon
-        Messages._print(
+        print(
             "get_monitor(): the monitor", name, "does not exist in this network."
         )
         return None
@@ -1263,7 +1263,7 @@ class Network(metaclass=NetworkMeta):
         for ext in self._data.extensions:
             if ext.name == name:
                 return ext
-        Messages._print(
+        print(
             "get_extension(): the extension", name, "does not exist in this network."
         )
         return None
@@ -1277,7 +1277,7 @@ class Network(metaclass=NetworkMeta):
         for constant in self._data.constants:
             if constant.name == name:
                 return constant
-        Messages._print(
+        print(
             "get_constant(): the constant", name, "does not exist in this network."
         )
         return None

ANNarchy/core/Population.py
@@ -316,7 +316,7 @@ class Population:
             try:
                 self.set(self.init)
             except Exception as e:
-                Messages._print(e)
+                print(e)
                 Messages._error(
                     "Population.reset(): something went wrong while resetting."
                 )
@@ -334,7 +334,7 @@ class Population:
                 try:
                     self.__setattr__(var, self.init[var])
                 except Exception as e:
-                    Messages._print(e)
+                    print(e)
                     Messages.warning(
                         "Population.reset(): something went wrong while resetting", var
                     )
@@ -444,7 +444,7 @@ class Population:
             else:
                 return getattr(self.cyInstance, attribute)
         except Exception as e:
-            Messages._print(e)
+            print(e)
             Messages._error(
                 " the variable "
                 + attribute
@@ -527,7 +527,7 @@ class Population:
                         setattr(self.cyInstance, attribute + "_host_to_device", True)
 
         except Exception as e:
-            Messages._print(e)
+            print(e)
             err_msg = """Population.set(): either the variable '%(attr)s' does not exist in the population '%(pop)s', or the provided array does not have the right size."""
             Messages._error(err_msg % {"attr": attribute, "pop": self.name})
 
@@ -1114,13 +1114,13 @@ class Population:
                 setattr(self.cyInstance, var, data)
 
             except Exception as e:
-                Messages._print(e)
-                Messages._print(var, data, type(data))
+                print(e)
+                print(var, data, type(data))
                 Messages.warning(
                     "Can not load the variable "
                     + var
                     + " in the population "
                     + self.name
                 )
-                Messages._print("Skipping this variable.")
+                print("Skipping this variable.")
                 continue

ANNarchy/core/Projection.py
@@ -870,7 +870,7 @@ class Projection:
             try:
                 self.__setattr__(var, self.init[var])
             except Exception as e:
-                Messages._print(e)
+                print(e)
                 Messages.warning(
                     "Projection.reset(): something went wrong while resetting", var
                 )
@@ -1549,7 +1549,7 @@ class Projection:
 
         if not path == "":
             if not os.path.isdir(path):
-                Messages._print("Creating folder", path)
+                print("Creating folder", path)
                 os.mkdir(path)
 
         extension = os.path.splitext(fname)[1]
@@ -1571,7 +1571,7 @@ class Projection:
 
         # Save the data
         if extension == ".gz":
-            Messages._print("Saving connectivity in gunzipped binary format...")
+            print("Saving connectivity in gunzipped binary format...")
             try:
                 import gzip
             except ImportError:
@@ -1581,16 +1581,16 @@ class Projection:
                 try:
                     pickle.dump(data, w_file, protocol=pickle.HIGHEST_PROTOCOL)
                 except Exception as e:
-                    Messages._print("Error while saving in gzipped binary format.")
-                    Messages._print(e)
+                    print("Error while saving in gzipped binary format.")
+                    print(e)
                     return
 
         elif extension == ".npz":
-            Messages._print("Saving connectivity in Numpy format...")
+            print("Saving connectivity in Numpy format...")
             np.savez_compressed(filename, **data)
 
         elif extension == ".mat":
-            Messages._print("Saving connectivity in Matlab format...")
+            print("Saving connectivity in Matlab format...")
             if data["delay"] is None:
                 data["delay"] = 0
             try:
@@ -1599,18 +1599,18 @@ class Projection:
                 sio.savemat(filename, data)
             except Exception as e:
                 Messages._error("Error while saving in Matlab format.")
-                Messages._print(e)
+                print(e)
                 return
 
         else:
-            Messages._print("Saving connectivity in text format...")
+            print("Saving connectivity in text format...")
             # save in Pythons pickle format
             with open(filename, mode="wb") as w_file:
                 try:
                     pickle.dump(data, w_file, protocol=pickle.HIGHEST_PROTOCOL)
                 except Exception as e:
-                    Messages._print("Error while saving in text format.")
-                    Messages._print(e)
+                    print("Error while saving in text format.")
+                    print(e)
                     return
             return
 
@@ -1800,7 +1800,7 @@ class Projection:
                     )(var)  # linear array or single constant
 
             except Exception as e:
-                Messages._print(e)
+                print(e)
                 Messages.warning(
                     "Can not save the attribute "
                     + var
@@ -1976,7 +1976,7 @@ class Projection:
                 try:
                     self._set_cython_attribute(var, desc[var])
                 except Exception as e:
-                    Messages._print(e)
+                    print(e)
                     Messages.warning(
                         "load(): the variable",
                         var,

commit c218aaf654821d0aa8646263b63f8a6ea8db0f82

bugfix (parser/report): statements like pos(mp) has been evaluated wrongly in the report. The error was introduced in commit f9ad29f.

ANNarchy/parser/report/LatexParser.py
@@ -77,7 +77,6 @@ def _process_neuron_equations(neuron, net_id):
         "Normal": sp.Function(r"\mathcal{N}"),
         "ite": sp.Function(r"ite", nargs=3),
     }
-
     for att in attributes:
         local_dict[att] = sp.Symbol(_latexify_name(att, variable_names))
 
@@ -410,7 +409,7 @@ class CustomLatexPrinter(LatexPrinter):
         For ite(), pos() and neg() only.
         """
         func = expr.func.__name__
-        args = [str(print(arg)) for arg in expr.args]
+        args = [self._print(arg) for arg in expr.args]
 
         if func == "ite":
             return r"""